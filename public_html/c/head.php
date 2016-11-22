16f4a7066a1b9231ed980abf4607c74a80af7385
monstersoft <pavillanueva@ing.ucsc.cl>
2016-11-22 05:59:04 -0300

    c

--- public_html/c/head.php
+++ public_html/c/head.php
@@ -1,7 +1,7 @@
 <meta charset="utf-8" />
 <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
-<meta name="theme-color" content="black">
+<meta name="theme-color" content="#262626">
 <link rel="stylesheet" type="text/css" class="ui" href="../../semantic/semantic.min.css">
 <link rel="stylesheet" href="../../toast/toast.css">
 <link rel="stylesheet" href="../../fontawesome/css/font-awesome.min.css">
