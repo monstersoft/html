d326c50611c75013f41b4da22b6bcb25c9a65848
monstersoft <pavillanueva@ing.ucsc.cl>
2016-11-22 06:25:04 -0300

    d

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
         <link rel="stylesheet" href="fontawesome/css/font-awesome.min.css">
         <style type="text/css">
             body {
-                background-color: #DADADA;
+                background-color: #262626;
             }
             .column {
                 max-width: 500px;
@@ -19,20 +19,24 @@
             .margen {
                 margin-top: 50px;
             }
+            #btnLogin {
+                background: #262626;
+                color: white;
+            }
         </style>
     </head>
     <body>
         <div class="ui aligned center aligned grid">
             <div class="margen column">
                 <h2 class="ui icon header">
-                    <i class="settings icon"></i>
-                    <div class="content teal">
+                    <i class="settings icon" style="color: #F5A214;"></i>
+                    <div class="content" style="color: white;">
                     Machine Monitors
-                    <div class="sub header">Plan de vigilancia de maquinaria pesada</div>
+                        <div class="sub header" style="color: white;">Plan de vigilancia de maquinaria pesada</div>
                     </div>
                 </h2>
                 <form class="ui form">
-                    <div class="ui segment">
+                    <div class="ui segment" >
                         <div class="field">
                             <div class="ui left icon input">
                                 <i class="user icon"></i>
@@ -53,7 +57,7 @@
                                 </div>
                             </div>
                           </div>
-                        <div id="btnLogin" class="ui fluid large black submit button">Ingresar</div>
+                        <div id="btnLogin" class="ui fluid large submit button">Ingresar</div>
                     </div>
                 </form>
                 <div class="ui message">
