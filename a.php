4791e251f483594bff3df33de32538388eb6ce5a
monstersoft <pavillanueva@ing.ucsc.cl>
2016-12-07 19:37:56 -0300

    s

--- a.php
+++ a.php
@@ -11,6 +11,7 @@
     <title>Document</title>
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <link rel="stylesheet" href="semantic/semantic.min.css">
+    <link rel="stylesheet" href="fontawesome/css/font-awesome.css">
 </head>
 <body>
   <div class="ui container">
@@ -19,7 +20,6 @@
         <table class="ui unstackable table">
           <thead>
             <tr>
-              <th class="center aligned">ID</th>
               <th class="center aligned">Empresa</th>
               <th class="center aligned">Proyectos</th>
               <th class="center aligned">Zonas</th>
@@ -33,13 +33,12 @@
               foreach ($empresas as $key => $value) {
                 echo ' 
                 <tr class="center aligned">
-                  <td>'.$value['idEmpresa'].'</td>
                   <td>'.$value['nombre'].'</td>
                   <td>'.$value['proyectos'].'</td>
                   <td>'.$value['zonas'].'</td>
                   <td>'.$value['maquinas'].'</td>
                   <td>'.$value['supervisores'].'</td>
-                  <td><a href="#">Editar</a>/<a href="#">Eliminar</a></td>
+                  <td><a  class="editar" href="#" id="'.$value['idEmpresa'].'">Editar</a>/<a  class="eliminar" href="#" id="'.$value['idEmpresa'].'">Eliminar</a></td>
                 </tr>
                 ';
               }
@@ -48,7 +47,7 @@
         </table>
       </div>
     </div>
-    <button class="ui mini circular teal icon button" id="insertar"><i class="icon add"></i></button>
+    <button class="ui mini circular teal icon button mas"><i class="icon add"></i></button>
   </div>
   <!--.............................................-->
   <div class="ui modal">
@@ -57,33 +56,33 @@
       Agregar Empresa
     </div>
     <div class="content">
-      <form class="ui form">
+      <form class="ui form" id="formulario" method="POST">
         <div class="field">
           <label>Nombre</label>
-          <input type="text" placeholder="Nombre empresa" id="nombre">
+          <input type="text" placeholder="Nombre empresa" name="nombre" value='patricio'>
         </div>
         <div class="field">
           <label>Rut</label>
-          <input type="text" placeholder="Rut empresa" name="rut">
+          <input type="text" placeholder="Rut empresa" name="rut" value="17286211-k">
         </div>
         <div class="field">
           <label>Correo</label>
-          <input type="text" placeholder="Correo electrónico" name="correo">
+          <input type="text" placeholder="Correo electrónico" name="correo" value='paaaa@gmail.com'>
         </div>
         <div class="field">
           <label>Teléfono</label>
-          <input type="text" placeholder="995007812" name="telefono">
+          <input type="text" placeholder="995007812" name="telefono" value="995007812">
         </div>
         <div class="field">
           <label>Dirección</label>
-          <input type="text" placeholder="Dirección empresa" name="direccion">
+          <input type="text" placeholder="Dirección empresa" name="direccion" value="jsjsjsjsjsj">
         </div>
         <div class="ui error message"></div>
         <div class="actions" style="text-align: right;">
             <div class="ui black button cancel reset cancelar">
                 <i class="remove icon"></i>Cancelar
             </div>
-            <button class="ui green button" type="submit"><i class="checkmark icon"></i>Añadir</button>
+            <button class="ui green button" type="submit" id="añadir"><i class="checkmark icon"></i>Añadir</button>
         </div>
       </form>
     </div>
@@ -93,14 +92,21 @@
   <script src="semantic/semantic.min.js"></script>
   <script>
     $(document).ready(function(){
-        $('#insertar').click(function(){
-          $('.ui.modal').modal('show');
+        $('.mas').click(function(){
+          $('.ui.form').trigger("reset");
+          $('.ui.form .field.error').removeClass( "error" );
+          $('.ui.form.error').removeClass( "error" );
+          $('.modal').modal('show');
         });
-        $('.reset').click(function(){
-          $('.ui.error.message').html('');
+        $('.editar').click(function(){
+          alert($(this).attr('id'));
         });
+        $('.eliminar').click(function(){
+          alert($(this).attr('id'));
+        })
     });
   </script>
   <script src="valida.js"></script>
+  <script src="js/msg.js"></script>
 </body>
 </html>
--- valida.js
+++ valida.js
@@ -1,41 +1,73 @@
 $(document).ready(function(){
 	$('.ui.form').form({
-		fields: {
-			nombre: {
-				identifier: 'nombre',
-				rules: [{
-					type: 'empty',
-					prompt: 'Tienes que ingresar el nombre de la empresa'
-				}]
-			},
-			rut: {
-				identifier: 'rut',
-				rules: [{
-					type: 'empty',
-					prompt: 'Tienes que ingresar el rut de la empresa'
-				}]
-			},
-			correo: {
-				identifier: 'correo',
-				rules: [{
-					type: 'regExp',
-					value: '/^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i',
-					prompt: 'El correo no está en un formato adecuado'
-				}]
-			},
-			telefono: {
-				identifier: 'telefono',
-				rules: [{
-					type: 'empty',
-					value: 'Tienes que ingresar el teléfono de la empresa'
-				},{
-					type: 'integer',
-					prompt: 'El teléfono tiene que ser un número'
-				},{
-					type: 'exactLength[9]',
-					prompt: 'El teléfono tiene que tener 9 dígitos'
-				}]
-			}
+		nombre: {
+			identifier: 'nombre',
+			rules: [{
+			  type: 'empty',
+			  prompt: 'Tienes que ingresar el nombre de la empresa'
+			}]
+		},
+		rut: {
+			identifier: 'rut',
+			rules: [{
+			  type: 'empty',
+			  prompt: 'Tienes que ingresar el rut de la empresa'
+			}]
+		},
+		correo: {
+			identifier: 'correo',
+			rules: [{
+			  type: 'regExp',
+			  value: '/^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i',
+			  prompt: 'El correo no está en un formato adecuado'
+			}]
+		},
+		telefono: {
+			identifier: 'telefono',
+			rules: [{
+			  type: 'empty',
+			  value: 'Tienes que ingresar el teléfono de la empresa'
+			},{
+			  type: 'integer',
+			  prompt: 'El teléfono tiene que ser un número'
+			},{
+			  type: 'exactLength[9]',
+			  prompt: 'El teléfono tiene que tener 9 dígitos'
+			}]
+		},
+		},{
+			onSuccess: function(e){
+				var datos = $('#formulario').serialize();
+                $.ajax({//INICIO PETICIÓN                     
+                url: 'hola.php',
+                data: datos,
+                type: "POST",
+                data: datos,
+                dataType: "json",
+                beforeSend: function() {
+                    $('#añadir').addClass('disabled');
+                    $('#añadir').addClass('ui loading button');
+                },
+                success: function(arreglo) {
+                    $(window).attr('location', arreglo.url);
+            }
+            }).fail(function( jqXHR, textStatus, errorThrown ){
+                if (jqXHR.status === 0){
+                    alert('No hay coneccion con el servidor');
+                } else if (jqXHR.status == 404) {
+                    alert('La pagina solicitada no fue encontrada, error 404');
+                } else if (jqXHR.status == 500) {
+                    alert('jqXHR.status == 500');
+                } else if (textStatus === 'parsererror') {
+                    alert('textStatus === parsererror');
+                } else if (textStatus === 'timeout') {
+                    alert('textStatus timeout');
+                } else if (textStatus === 'abort') {
+                    alert('La peticion fue abortada');
+                } else {
+                    alert('Error desconocido');
+                }
+            });//FIN PETICION AJAX
 		}
 	});
 });
