4c58ae426c97823ca921bc0d9b9b5d632aa21d0e
monstersoft <pavillanueva@ing.ucsc.cl>
2016-12-15 01:44:05 -0300

    5

--- public_html/c/verEmpresas.php
+++ public_html/c/verEmpresas.php
@@ -9,7 +9,7 @@
         <div class="pusher">
         <?php require_once 'contenido/barra.php'; ?>
             <div class="ui grid">
-                <!--CONTENIDO ..............................................................................-->
+<!--CONTENIDO ..............................................................................-->
                 <div class="ui sixteen wide mobile sixteen wide tablet  four wide computer column">
                     <div class="ui fluid card">
                         <div class="content">
@@ -68,17 +68,251 @@
                         </div>
                     </div>
                 </div>
-            </div>
-        </div>
+                <div class="ui sixteen wide mobile sixteen wide tablet  four wide computer column">
+                    <div class="ui fluid card">
+                        <div class="content">
+                            <i class="industry icon right floated"></i>
+                            <div class="header">Los Pirineos</div>
+                            <div class="ui divider"></div>
+                            <div class="description">
+                                <table class="ui very basic unstackable table responsive">
+                                <thead>
+                                    <tr>
+                                        <th class="center aligned">Patente</th>
+                                        <th class="center aligned">Fecha de registro</th>
+                                        <th class="center aligned">Velocidad máxima</th>
+                                        <th class="center aligned">Tara</th>
+                                        <th class="center aligned">Carga máxima</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td class="center aligned">A1B233</td>
+                                        <td class="center aligned">10/11/12</td>
+                                        <td class="center aligned">10 km/hr</td>
+                                        <td class="center aligned">50 kg</td>
+                                        <td class="center aligned">100 kg</td>
+                                    </tr>
+                                    <tr>
+                                        <td class="center aligned">A1B233</td>
+                                        <td class="center aligned">10/11/12</td>
+                                        <td class="center aligned">10 km/hr</td>
+                                        <td class="center aligned">50 kg</td>
+                                        <td class="center aligned">100 kg</td>
+                                    </tr>                                   
+                                </tbody>
+                                </table>
+                                <div class="ui divider"></div>
+                                <div class="ui relaxed divided list">
+                                    <div class="item">
+                                        <i class="large user middle aligned icon"></i>
+                                        <div class="content">
+                                            <a class="header">Juanito Pérez Pérez</a>
+                                            <div class="description">jperez@serviciosbiobio.cl</div>
+                                            <div class="description">Fono: 995007812</div>
+                                        </div>
+                                    </div>
+                                    <div class="item">
+                                        <i class="large user middle aligned icon"></i>
+                                        <div class="content">
+                                            <a class="header">Juanito Pérez Pérez</a>
+                                            <div class="description">jperez@serviciosbiobio.cl</div>
+                                            <div class="description">Fono: 995007812</div>
+                                        </div>
+                                    </div>
+                                    <div class="item">
+                                        <i class="large user middle aligned icon"></i>
+                                        <div class="content">
+                                            <a class="header">Juanito Pérez Pérez</a>
+                                            <div class="description">jperez@serviciosbiobio.cl</div>
+                                            <div class="description">Fono: 995007812</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <!--CONTENIDO ..............................................................................-->
-                <?php require_once 'contenido/modalEmpresa.php' ?>
-            </div>
-        </div>
+                <div class="ui sixteen wide mobile sixteen wide tablet  four wide computer column">
+                    <div class="ui fluid card">
+                        <div class="content">
+                            <i class="industry icon right floated"></i>
+                            <div class="header">Los Lirios</div>
+                            <div class="ui divider"></div>
+                            <div class="description">
+                                <table class="ui very basic unstackable table responsive">
+                                <thead>
+                                    <tr>
+                                        <th class="center aligned">Patente</th>
+                                        <th class="center aligned">Fecha de registro</th>
+                                        <th class="center aligned">Velocidad máxima</th>
+                                        <th class="center aligned">Tara</th>
+                                        <th class="center aligned">Carga máxima</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td class="center aligned">A1B233</td>
+                                        <td class="center aligned">10/11/12</td>
+                                        <td class="center aligned">10 km/hr</td>
+                                        <td class="center aligned">50 kg</td>
+                                        <td class="center aligned">100 kg</td>
+                                    </tr>
+                                    <tr>
+                                        <td class="center aligned">A1B233</td>
+                                        <td class="center aligned">10/11/12</td>
+                                        <td class="center aligned">10 km/hr</td>
+                                        <td class="center aligned">50 kg</td>
+                                        <td class="center aligned">100 kg</td>
+                                    </tr>
+                                    <tr>
+                                        <td class="center aligned">A1B233</td>
+                                        <td class="center aligned">10/11/12</td>
+                                        <td class="center aligned">10 km/hr</td>
+                                        <td class="center aligned">50 kg</td>
+                                        <td class="center aligned">100 kg</td>
+                                    </tr>
+                                    <tr>
+                                        <td class="center aligned">A1B233</td>
+                                        <td class="center aligned">10/11/12</td>
+                                        <td class="center aligned">10 km/hr</td>
+                                        <td class="center aligned">50 kg</td>
+                                        <td class="center aligned">100 kg</td>
+                                    </tr>
+                                </tbody>
+                                </table>
+                                <div class="ui divider"></div>
+                                <div class="ui relaxed divided list">
+                                    <div class="item">
+                                        <i class="large user middle aligned icon"></i>
+                                        <div class="content">
+                                            <a class="header">Juanito Pérez Pérez</a>
+                                            <div class="description">jperez@serviciosbiobio.cl</div>
+                                            <div class="description">Fono: 995007812</div>
+                                        </div>
+                                    </div>
+                                    <div class="item">
+                                        <i class="large user middle aligned icon"></i>
+                                        <div class="content">
+                                            <a class="header">Juanito Pérez Pérez</a>
+                                            <div class="description">jperez@serviciosbiobio.cl</div>
+                                            <div class="description">Fono: 995007812</div>
+                                        </div>
+                                    </div>
+                                    <div class="item">
+                                        <i class="large user middle aligned icon"></i>
+                                        <div class="content">
+                                            <a class="header">Juanito Pérez Pérez</a>
+                                            <div class="description">jperez@serviciosbiobio.cl</div>
+                                            <div class="description">Fono: 995007812</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="ui sixteen wide mobile sixteen wide tablet  four wide computer column">
+                    <div class="ui fluid card">
+                        <div class="content">
+                            <i class="industry icon right floated"></i>
+                            <div class="header">Los Robles</div>
+                            <div class="ui divider"></div>
+                            <div class="description">
+                                <table class="ui very basic unstackable table responsive">
+                                <thead>
+                                    <tr>
+                                        <th class="center aligned">Patente</th>
+                                        <th class="center aligned">Fecha de registro</th>
+                                        <th class="center aligned">Velocidad máxima</th>
+                                        <th class="center aligned">Tara</th>
+                                        <th class="center aligned">Carga máxima</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td class="center aligned">A1B233</td>
+                                        <td class="center aligned">10/11/12</td>
+                                        <td class="center aligned">10 km/hr</td>
+                                        <td class="center aligned">50 kg</td>
+                                        <td class="center aligned">100 kg</td>
+                                    </tr>
+                                    <tr>
+                                        <td class="center aligned">A1B233</td>
+                                        <td class="center aligned">10/11/12</td>
+                                        <td class="center aligned">10 km/hr</td>
+                                        <td class="center aligned">50 kg</td>
+                                        <td class="center aligned">100 kg</td>
+                                    </tr>
+                                    <tr>
+                                        <td class="center aligned">A1B233</td>
+                                        <td class="center aligned">10/11/12</td>
+                                        <td class="center aligned">10 km/hr</td>
+                                        <td class="center aligned">50 kg</td>
+                                        <td class="center aligned">100 kg</td>
+                                    </tr>
+                                    <tr>
+                                        <td class="center aligned">A1B233</td>
+                                        <td class="center aligned">10/11/12</td>
+                                        <td class="center aligned">10 km/hr</td>
+                                        <td class="center aligned">50 kg</td>
+                                        <td class="center aligned">100 kg</td>
+                                    </tr>
+                                    <tr>
+                                        <td class="center aligned">A1B233</td>
+                                        <td class="center aligned">10/11/12</td>
+                                        <td class="center aligned">10 km/hr</td>
+                                        <td class="center aligned">50 kg</td>
+                                        <td class="center aligned">100 kg</td>
+                                    </tr>
+                                    <tr>
+                                        <td class="center aligned">A1B233</td>
+                                        <td class="center aligned">10/11/12</td>
+                                        <td class="center aligned">10 km/hr</td>
+                                        <td class="center aligned">50 kg</td>
+                                        <td class="center aligned">100 kg</td>
+                                    </tr>
+                                </tbody>
+                                </table>
+                                <div class="ui divider"></div>
+                                <div class="ui relaxed divided list">
+                                    <div class="item">
+                                        <i class="large user middle aligned icon"></i>
+                                        <div class="content">
+                                            <a class="header">Juanito Pérez Pérez</a>
+                                            <div class="description">jperez@serviciosbiobio.cl</div>
+                                            <div class="description">Fono: 995007812</div>
+                                        </div>
+                                    </div>
+                                    <div class="item">
+                                        <i class="large user middle aligned icon"></i>
+                                        <div class="content">
+                                            <a class="header">Juanito Pérez Pérez</a>
+                                            <div class="description">jperez@serviciosbiobio.cl</div>
+                                            <div class="description">Fono: 995007812</div>
+                                        </div>
+                                    </div>
+                                    <div class="item">
+                                        <i class="large user middle aligned icon"></i>
+                                        <div class="content">
+                                            <a class="header">Juanito Pérez Pérez</a>
+                                            <div class="description">jperez@serviciosbiobio.cl</div>
+                                            <div class="description">Fono: 995007812</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+<!--CONTENIDO ..............................................................................-->
+            </div><!--FIN GRID-->
+        </div><!--FIN PUSHER-->
+        <?php require_once 'contenido/modalEmpresa.php' ?>
         <?php require_once 'contenido/script.php'; ?>
         <script src="../../js/modalEmpresa.js"></script>
         <script src="../../js/jquery.rut.chileno.js"></script>
-                <script src="../../js/responsive-tables.js"></script>
+        <script src="../../js/responsive-tables.js"></script>
         <script>
             $('.ui.accordion')
   .accordion()
