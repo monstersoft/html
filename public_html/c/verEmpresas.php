c2d5b214dcebe5cc4c42382c16b0a5b212c2a9f5
monstersoft <pavillanueva@ing.ucsc.cl>
2016-12-15 00:18:53 -0300

    s

--- public_html/c/verEmpresas.php
+++ public_html/c/verEmpresas.php
@@ -10,61 +10,79 @@
             <div class="ui grid">
                 <!--CONTENIDO ..............................................................................-->
                                 <div class="ui sixteen wide mobile sixteen wide tablet  eight wide computer column">
-<table class="ui unstackable table">
-  <thead>
-    <tr>
-      <th>Name</th>
-      <th>Status</th>
-      <th class="right aligned">Notes</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <td>John</td>
-      <td>Approved</td>
-      <td class="right aligned">None</td>
-    </tr>
-    <tr>
-      <td>Jamie</td>
-      <td>Approved</td>
-      <td class="right aligned">Requires call</td>
-    </tr>
-    <tr>
-      <td>Jill</td>
-      <td>Denied</td>
-      <td class="right aligned">None</td>
-    </tr>
-  </tbody>
-</table>
-<table class="ui tablet stackable table">
-  <thead>
-    <tr>
-      <th>Name</th>
-      <th>Status</th>
-      <th class="right aligned">Notes</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <td>John</td>
-      <td>Approved</td>
-      <td class="right aligned">None</td>
-    </tr>
-    <tr>
-      <td>Jamie</td>
-      <td>Approved</td>
-      <td class="right aligned">Requires call</td>
-    </tr>
-    <tr>
-      <td>Jill</td>
-      <td>Denied</td>
-      <td class="right aligned">None</td>
-    </tr>
-  </tbody>
-</table>
+                    <div class="ui fluid card">
+                        <div class="content">
+                            <i class="industry icon right floated"></i>
+                            <div class="header">Los Acacios</div>
+                            <div class="ui divider"></div>
+                            <div class="description">
+                            <table class="ui very basic unstackable table">
+                              <thead>
+                                <tr>
+                                  <th>Patente</th>
+                                  <th>Fecha de registro</th>
+                                  <th class="right aligned">Velocidad máxima</th>
+                                  <th class="right aligned">Tara</th>
+                                  <th class="right aligned">Carga máxima</th>
+                                </tr>
+                              </thead>
+                              <tbody>
+                                <tr>
+                                  <td>A1B233</td>
+                                  <td>10/11/12</td>
+                                  <td class="right aligned">10 km/hr</td>
+                                  <td class="right aligned">50 kg</td>
+                                  <td class="right aligned">100 kg</td>
+                                </tr>
+                                <tr>
+                                  <td>A1B233</td>
+                                  <td>10/11/12</td>
+                                  <td class="right aligned">10 km/hr</td>
+                                  <td class="right aligned">50 kg</td>
+                                  <td class="right aligned">100 kg</td>
+                                </tr>
+                                <tr>
+                                  <td>A1B233</td>
+                                  <td>10/11/12</td>
+                                  <td class="right aligned">10 km/hr</td>
+                                  <td class="right aligned">50 kg</td>
+                                  <td class="right aligned">100 kg</td>
+                                </tr>
+                              </tbody>
+                            </table>
+                            <div class="ui divider"></div>
+                            <div class="ui relaxed divided list">
+                              <div class="item">
+                                <i class="large user middle aligned icon"></i>
+                                <div class="content">
+                                  <a class="header">Juanito Pérez Pérez</a>
+                                  <div class="description">jperez@serviciosbiobio.cl</div>
+                                  <div class="description">Fono: 995007812</div>
+                                </div>
+                              </div>
+                              <div class="item">
+                                <i class="large user middle aligned icon"></i>
+                                <div class="content">
+                                  <a class="header">Juanito Pérez Pérez</a>
+                                  <div class="description">jperez@serviciosbiobio.cl</div>
+                                  <div class="description">Fono: 995007812</div>
+                                </div>
+                              </div>
+                              <div class="item">
+                                <i class="large user middle aligned icon"></i>
+                                <div class="content">
+                                  <a class="header">Juanito Pérez Pérez</a>
+                                  <div class="description">jperez@serviciosbiobio.cl</div>
+                                  <div class="description">Fono: 995007812</div>
+                                </div>
+                              </div>
+                            </div>
                             </div>
                         </div>
-                        <a class="ui bottom attached button insertar" href="#"><i class="plus icon"></i></a>
+                    </div>
+                </div>
+
+                        </div>
                     </div>
                 </div>
                 <!--CONTENIDO ..............................................................................-->
