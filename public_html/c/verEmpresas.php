f4a620de4288ec74a231c60b81da06a24f745eed
monstersoft <pavillanueva@ing.ucsc.cl>
2016-12-14 23:44:07 -0300

    d

--- public_html/c/verEmpresas.php
+++ public_html/c/verEmpresas.php
@@ -68,6 +68,37 @@
     </tr>
   </tbody>
 </table>
+<table class="ui collapsing table">
+  <thead>
+    <tr>
+      <th>Name</th>
+      <th>Status</th>
+      <th>Notes</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>John</td>
+      <td>Approved</td>
+      <td>None</td>
+    </tr>
+    <tr>
+      <td>Jamie</td>
+      <td>Approved</td>
+      <td>Requires call</td>
+    </tr>
+    <tr>
+      <td>Jill</td>
+      <td>Denied</td>
+      <td>None</td>
+    </tr>
+  </tbody>
+  <tfoot>
+    <tr><th>3 People</th>
+    <th>2 Approved</th>
+    <th></th>
+  </tr></tfoot>
+</table>
                             </div>
                         </div>
                         <a class="ui bottom attached button insertar" href="#"><i class="plus icon"></i></a>
