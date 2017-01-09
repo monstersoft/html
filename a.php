5387d7d470dffc4343f299954e2446b62730c3a3
monstersoft <pavillanueva@ing.ucsc.cl>
2017-01-09 03:54:46 -0300

    de

--- a.php
+++ a.php
@@ -1,5 +1,10 @@
-<?php
-	$datosJson = $_POST['datos'];
-	$arreglo['mensaje'] = $datosJson[1]['nombre']['original'];
-	echo json_encode($arreglo);
+<?php 
+$paises = $_POST['country']; 
+if(is_array($paises)) {
+        $t = sizeof($paises);
+        $arreglo['mensaje'] = 'Es un array de tamanho: '.$t;
+    }
+else 
+    $arreglo['mensaje'] = 'No es un array';
+echo json_encode($arreglo);
 ?>
