3362b804a6d7a3416590b5daa4a22286c2f81fc2
monstersoft <pavillanueva@ing.ucsc.cl>
2017-02-06 22:27:22 -0800

    dsddsdX

--- php/funciones.php
+++ php/funciones.php
@@ -1,71 +1,6 @@
 <?php
 	include("conexion.php");
 
-	function enviarMailRegistroSupervisor($nombreSupervisor,$emailSupervisor,$link) {
-		require '../mailer/class.phpmailer.php';
-		$e = new PHPMailer;
-		$e->Host = 'localhost';
-		$e->From = "machmonitor@gmail.com";
-		$e->FromName = 'Machine Monitors';
-		$e->Subject = 'Registro de Supervisores';
-		$e->addAddress($emailSupervisor);
-		$e->MsgHTML('<!DOCTYPE html>
-						<html>
-						    <head>
-						    	<meta charset="UTF-8">
-						    	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-								<style>
-									* {
-										margin: 0;
-										padding: 0;
-										box-sizing: border-box;
-									}
-									body {
-										font-family: Arial;
-									}
-									.letra {
-										font-size: 20px;
-										font-weight: 100;
-										color: #fff;
-									}
-						            .contenedor {
-						                width: 100%;
-						                margin: 0 auto;
-						                overflow: hidden;
-						            }
-						            .rectangulo {
-						                text-align: center;
-						                float: left;
-						                padding: 10px;
-										border-bottom: 5px solid #F5A214;
-										width: 100%;
-										height: 45px;
-										background: #262626;
-						            }
-						            .cuadrado {
-						                width: 100%;
-						                float: left;
-						                padding: 20px 0px 20px 0px;
-						                border-bottom: 5px solid #F5A214;
-						                font-size: 15px;
-						            }
-						        </style>
-						    </head>
-						    <body>
-						    	<div class="contenedor">
-						    		<div class="rectangulo">
-						                <p class="letra">Machine Monitors</p>
-						            </div>
-						    		<div class="cuadrado">Estimado '.$nombreSupervisor.', para finalizar el registro debes ingresar al siguiente enlance y completar el formulario<br><br><a href='.$link.'>'.$link.'</a><div>
-						        </div>
-						    </body>
-						</html>');
-		if($e->Send()) {
-			return true;
-			}
-		else
-			return false;
-	}
 	
     function cantidadMaquinas($idZona) {
         $conexion = conectar();
@@ -422,6 +357,71 @@
 	        }
 	    });
     return $array;
+	}
+function enviarMailRegistroSupervisor($nombreSupervisor,$emailSupervisor,$link) {
+		require '../mailer/class.phpmailer.php';
+		$e = new PHPMailer;
+		$e->Host = 'localhost';
+		$e->From = "machmonitor@gmail.com";
+		$e->FromName = 'Machine Monitors';
+		$e->Subject = 'Registro de Supervisores';
+		$e->addAddress($emailSupervisor);
+		$e->MsgHTML('<!DOCTYPE html>
+						<html>
+						    <head>
+						    	<meta charset="UTF-8">
+						    	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+								<style>
+									* {
+										margin: 0;
+										padding: 0;
+										box-sizing: border-box;
+									}
+									body {
+										font-family: Arial;
+									}
+									.letra {
+										font-size: 20px;
+										font-weight: 100;
+										color: #fff;
+									}
+						            .contenedor {
+						                width: 100%;
+						                margin: 0 auto;
+						                overflow: hidden;
+						            }
+						            .rectangulo {
+						                text-align: center;
+						                float: left;
+						                padding: 10px;
+										border-bottom: 5px solid #F5A214;
+										width: 100%;
+										height: 45px;
+										background: #262626;
+						            }
+						            .cuadrado {
+						                width: 100%;
+						                float: left;
+						                padding: 20px 0px 20px 0px;
+						                border-bottom: 5px solid #F5A214;
+						                font-size: 15px;
+						            }
+						        </style>
+						    </head>
+						    <body>
+						    	<div class="contenedor">
+						    		<div class="rectangulo">
+						                <p class="letra">Machine Monitors</p>
+						            </div>
+						    		<div class="cuadrado">Estimado '.$nombreSupervisor.', para finalizar el registro debes ingresar al siguiente enlance y completar el formulario<br><br><a href='.$link.'>'.$link.'</a><div>
+						        </div>
+						    </body>
+						</html>');
+		if($e->Send()) {
+			return true;
+			}
+		else
+			return false;
 	}
 
 ?>
