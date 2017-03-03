435472facaf3e7bb9200c0193af397fb32c6481f
monstersoft <pavillanueva@ing.ucsc.cl>
2017-03-02 17:01:31 -0800

    j

---
+++ e/b.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="theme-color" content="#262626">
+    <title>Simulador de datos</title>
+    <link rel="stylesheet" href="semantic.css">
+    <link rel="stylesheet" href="../cliente/css/panel.css">
+    <style>
+        /** {
+            margin: 0 !important;
+            padding: 0 !important;
+            box-sizing: border-box !important;
+        }*/
+        .b {
+            border: 1px solid #262626;
+        }
+    </style>
+</head>
+<body>
+    <div class="ui grid container b">
+        <div class="sixteen wide mobile column b">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi autem aut quae, facilis soluta consequuntur ut perspiciatis? Architecto nam veniam rem quisquam, autem velit, hic unde facere cupiditate, delectus veritatis.>
+        </div>
+    </div>
+<div class="ui two column grid b">
+  <div class="column">
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate atque, similique accusamus. Eaque, amet, ducimus. Repudiandae, nam aperiam deleniti magnam iure, non! Unde rem explicabo perferendis quisquam! At, aspernatur, mollitia.</p>
+  </div>
+  <div class="column">
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum iusto ad necessitatibus ipsa repellat incidunt, nulla nisi sapiente quasi, temporibus excepturi saepe ab atque distinctio quae tempore fugit! Sunt, explicabo!</p>
+  </div>
+</div>
+<div class="ui two column padded grid b">
+  <div class="column">
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate atque, similique accusamus. Eaque, amet, ducimus. Repudiandae, nam aperiam deleniti magnam iure, non! Unde rem explicabo perferendis quisquam! At, aspernatur, mollitia.</p>
+  </div>
+  <div class="column">
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum iusto ad necessitatibus ipsa repellat incidunt, nulla nisi sapiente quasi, temporibus excepturi saepe ab atque distinctio quae tempore fugit! Sunt, explicabo!</p>
+  </div>
+</div>
+    <script src="jquery2.js"></script>
+    <script src="semantic.js"></script>
+    <script src="chart.min.js"></script>
+</body>
+</html>
