7af597f8925a9a6b76608b92611042f741a2210d
monstersoft <pavillanueva@ing.ucsc.cl>
2017-03-09 01:43:55 -0800

    c

---
+++ a.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
+        <meta name="theme-color" content="#DADADA" />
+        <title>Machine Monitors</title>
+        <link rel="stylesheet" href="semantic/semantic.css">
+        <style>
+            * {
+                padding: 0 !important;
+                margin: 0 !important;
+                box-sizing: border-box !important;
+            }
+            .ui.grid {
+                margin: 0;
+            }
+            .borde {
+                border: 1px solid #F5A214;
+            }
+        </style>
+    </head>
+    <body>
+    <div class="ui grid">
+<div class="ui eight wide mobile column">
+  <div class="sixteen wide mobile column borde">Empresa <i class="industry icon right floated"></i></div>
+     
+     <div class="ui grid">
+      <div class="equal width row">
+        <div class="column borde">
+            <h6 class="ui center aligned icon header">
+                <i class="map icon"></i>
+                <div class="content">ZONAS
+                    <div class="sub header">7</div>
+                </div>
+            </h6>
+        </div>
+        <div class="column borde">
+            <h6 class="ui center aligned icon header">
+                <i class="setting icon"></i>
+                <div class="content">MAQUINAS
+                    <div class="sub header">10</div>
+                </div>
+            </h6>
+        </div>
+        <div class="column borde">
+            <h6 class="ui center aligned icon header">
+                <i class="users icon"></i>
+                <div class="content">SUPERVISORES
+                    <div class="sub header">10</div>
+                </div>
+            </h6>
+        </div>
+      </div>
+      </div>
+</div>
+       </div>
+        <script src="js/jquery/jquery2.js"></script>
+        <script src="semantic/semantic.js"></script>
+    </body>
+</html>
