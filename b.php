627ab1d6908088350e8063a25b6ef035d5353daa
monstersoft <pavillanueva@ing.ucsc.cl>
2017-03-09 17:48:56 -0800

    c

---
+++ b.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
+        <meta name="theme-color" content="#DADADA" />
+        <title>Machine Monitors</title>
+        <link rel="stylesheet" href="semantic/semantic.css">
+    </head>
+    <body>
+<body>
+  <div class="ui sidebar inverted vertical menu">
+    <a class="item">
+      Pato
+    </a>
+    <a class="item">
+      Javier
+    </a>
+    <a class="item">
+      Cecilia
+    </a>
+    <a class="item">
+      Pato
+    </a>
+    <a class="item">
+      Javier
+    </a>
+    <a class="item">
+      Cecilia
+    </a>
+  </div>
+  <div class="pusher">
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil voluptatum odit dolorum obcaecati nostrum quaerat, fugiat repellat debitis? Amet non eum vel officiis. Repellat magni excepturi soluta sint, tempore, fugiat!
+  </div>
+</body>
+       </div>
+        <script src="js/jquery/jquery2.js"></script>
+        <script src="semantic/semantic.js"></script>
+        <script>
+        $('.ui.sidebar')
+  .sidebar('toggle')
+;
+        </script>
+    </body>
+</html>
