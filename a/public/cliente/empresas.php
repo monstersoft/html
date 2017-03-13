187e996bb2fb075bd43b5243c66cd21e2369ee4c
monstersoft <pavillanueva@ing.ucsc.cl>
2017-03-13 16:14:14 -0300

    s

--- a/public/cliente/empresas.php
+++ a/public/cliente/empresas.php
@@ -22,10 +22,11 @@
     <link rel="stylesheet" href="../../css/awesome/css/font-awesome.css">
     <link rel="stylesheet" href="../../css/panel.css">
     <link rel="stylesheet" href="../../css/empresas.css">
+    <link rel="stylesheet" href="../../css/animate.css">
 </head>
 <body>
     <div id="bar"><a id="clickMenu"><i class="fa fa-bars"></i></a><p>Machine Monitors</p></div>
-    <nav class="unDisplayNav">
+    <nav class="unDisplayNav animated slideInLeft">
         <ul>
             <li id="profile"><i class="fa fa-cogs fa-4x" id="iconProfile"></i><br><span id="titleProfile"><?php  echo $perfil['empresa'] ?></span><br><span id="nameProfile"><?php echo $perfil['correo']; ?></span></li>
             <li><a href="#"><i class="fa fa-tachometer icons"></i>Dashboard</a></li>
@@ -36,7 +37,7 @@
             <li><a href="#"><i class="fa fa-sign-out icons"></i>Cerrar</a></li>
         </ul>
     </nav>
-    <div id="content" class="unLeftContent"> 
+    <div id="content" class="unLeftContent animated slideInLeft"> 
         <div class="col-xs-12 col-sm-6 card">
             <div class="col-xs-12 shadow">
                 <div class="col-xs-12 titleIndustry">
