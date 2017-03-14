89dd1fe794a31e506b37d40199894d4d676713f4
monstersoft <pavillanueva@ing.ucsc.cl>
2017-03-14 00:35:58 -0300

    S

--- a/js/funciones.js
+++ a/js/funciones.js
@@ -1,99 +1,107 @@
 function main () {
-    var shadow = false;
-    //WHEN THE PAGE STARTS
-    if($(window).width() > 992) {
-        if($('nav').hasClass('unDisplayNav'))
-            $('nav').removeClass('unDisplayNav');
-        if($('#content').hasClass('unLeftContent'))
-            $('#content').removeClass('unLeftContent');
-        $('nav').addClass('displayNav')
-        $('#content').addClass('leftContent');
-        $('#content').css('width','calc(100% - 250px)');
-    }
-    //WHEN DOES THE RESIZE
-    $(window).resize(function(){
-        if($(window).width() > 992) {
-            if(shadow == true) {
-                $('#shadowDisplay').remove();
-                shadow = false;
-            }
-            if($('nav').hasClass('unDisplayNav'))
-                $('nav').removeClass('unDisplayNav');
-            if($('#content').hasClass('unLeftContent'))
-                $('#content').removeClass('unLeftContent');
-            $('nav').addClass('displayNav');
-            $('#content').addClass('leftContent');
-            $('#content').css('width','calc(100% - 250px)');
-        }
-        if($(window).width() <= 992) {
-            if(shadow == true) {
-                $('#shadowDisplay').remove();
-                shadow = false;
-            }
-            if($('nav').hasClass('displayNav'))
-                $('nav').removeClass('displayNav');
-            if($('#content').hasClass('leftContent'))
-                $('#content').removeClass('leftContent');
-            $('nav').addClass('unDisplayNav');
-            $('#content').addClass('unLeftContent');
-            $('#content').css('width','100%');
-        }   
-    });
-    //WHEN CLICK
-    $('#clickMenu').click(function(){
-        if($(window).width() > 992) {
-            if($('nav').hasClass('displayNav') && $('#content').hasClass('leftContent')) {
-                $('nav').removeClass('displayNav');
-                $('#content').removeClass('leftContent');
-                $('nav').addClass('unDisplayNav');
-                $('#content').addClass('unLeftContent');
-                $('#content').css('width','100%');
-            }
-            else {
-                $('nav').removeClass('unDisplayNav');
-                $('#content').removeClass('unLeftContent');
-                $('nav').addClass('displayNav');
-                $('#content').addClass('leftContent');
-                $('#content').css('width','calc(100% - 250px)');
-            }
-        }    
-        if($(window).width() <= 992) {
-            if(shadow == true) {
-                $('nav').removeClass('displayNav');
-                $('#content').removeClass('leftContent');
-                $('nav').addClass('unDisplayNav');
-                $('#content').addClass('unLeftContent');
-                $('#content').css('width','100%');
-                $('#shadowDisplay').remove();
-                shadow = false;
-            }
-            else {
-                if($('nav').hasClass('unDisplayNav') && $('#content').hasClass('unLeftContent')) {
-                    $('nav').removeClass('unDisplayNav');
-                    $('#content').removeClass('unLeftContent');
-                    $('nav').addClass('displayNav');
-                    $('#content').addClass('leftContent');
-                    $('#content').css('width','100%');
-                    $('#content').after('<div id="shadowDisplay" class="shadowDisplay"></div>');
-                    shadow = true;
-                }
-                else {
-                    $('nav').removeClass('displayNav');
-                    $('#content').removeClass('leftContent');
-                    $('nav').addClass('unDisplayNav');
-                    $('#content').addClass('unLeftContent');
-                    $('#content').css('width','100%');
-                }
-            }
-        }
-    });
-    $('body').on('click','#shadowDisplay',function(){
-        $(this).remove();
-        $('nav').removeClass('displayNav');
-        $('#content').removeClass('leftContent');
-        $('nav').addClass('unDisplayNav');
-        $('#content').addClass('unLeftContent');
-        $('#content').css('width','100%');
-        shadow = false;
-    });
+		var shadow = false;
+		//WHEN THE PAGE STARTS
+		if($(window).width() > 992) {
+				if($('nav').hasClass('unDisplayNav'))
+						$('nav').removeClass('unDisplayNav');
+				if($('#content').hasClass('unLeftContent'))
+						$('#content').removeClass('unLeftContent');
+				$('nav').addClass('displayNav')
+				$('#content').addClass('leftContent');
+				$('#content').css('width','calc(100% - 250px)');
+		}
+		//WHEN DOES THE RESIZE
+		$(window).resize(function(){
+				if($(window).width() > 992) {
+						if(shadow == true) {
+								$('#shadowDisplay').remove();
+								shadow = false;
+						}
+						if($('nav').hasClass('unDisplayNav'))
+								$('nav').removeClass('unDisplayNav');
+						if($('#content').hasClass('unLeftContent'))
+								$('#content').removeClass('unLeftContent');
+						$('nav').addClass('displayNav');
+						$('#content').addClass('leftContent');
+						$('#content').css('width','calc(100% - 250px)');
+				}
+				if($(window).width() <= 992) {
+						if(shadow == true) {
+								$('#shadowDisplay').remove();
+								shadow = false;
+						}
+						if($('nav').hasClass('displayNav'))
+								$('nav').removeClass('displayNav');
+						if($('#content').hasClass('leftContent'))
+								$('#content').removeClass('leftContent');
+						$('nav').addClass('unDisplayNav');
+						$('#content').addClass('unLeftContent');
+						$('#content').css('width','100%');
+				}
+		});
+		//WHEN CLICK
+		$('#clickMenu').click(function(){
+				if($(window).width() > 992) {
+						if($('nav').hasClass('displayNav') && $('#content').hasClass('leftContent')) {
+								$('nav').removeClass('displayNav');
+								$('#content').removeClass('leftContent');
+								$('nav').addClass('unDisplayNav');
+								$('#content').addClass('unLeftContent');
+								$('#content').css('width','100%');
+						}
+						else {
+								$('nav').removeClass('unDisplayNav');
+								$('#content').removeClass('unLeftContent');
+								$('nav').addClass('displayNav');
+								$('#content').addClass('leftContent');
+								$('#content').css('width','calc(100% - 250px)');
+						}
+				}
+				if($(window).width() <= 992) {
+						if(shadow == true) {
+								$('nav').removeClass('displayNav');
+								$('#content').removeClass('leftContent');
+								$('nav').addClass('unDisplayNav');
+								$('#content').addClass('unLeftContent');
+								$('#content').css('width','100%');
+								$('#shadowDisplay').remove();
+								shadow = false;
+						}
+						else {
+								if($('nav').hasClass('unDisplayNav') && $('#content').hasClass('unLeftContent')) {
+										$('nav').removeClass('unDisplayNav');
+										$('#content').removeClass('unLeftContent');
+										$('nav').addClass('displayNav');
+										$('#content').addClass('leftContent');
+										$('#content').css('width','100%');
+										$('#content').after('<div id="shadowDisplay" class="shadowDisplay"></div>');
+										shadow = true;
+								}
+								else {
+										$('nav').removeClass('displayNav');
+										$('#content').removeClass('leftContent');
+										$('nav').addClass('unDisplayNav');
+										$('#content').addClass('unLeftContent');
+										$('#content').css('width','100%');
+								}
+						}
+				}
+		});
+		$('body').on('click','#shadowDisplay',function(){
+				$(this).remove();
+				$('nav').removeClass('displayNav');
+				$('#content').removeClass('leftContent');
+				$('nav').addClass('unDisplayNav');
+				$('#content').addClass('unLeftContent');
+				$('#content').css('width','100%');
+				shadow = false;
+		});
+}
+function modals () {
+		$('.btnEditBusiness').click(function(){
+				$('#modalEditBusiness').modal();
+		});
+		$('.btnRemoveBusiness').click(function(){
+				$('#modalRemoveBusiness').modal();
+		});
 }
--- a/public/cliente/empresas.php
+++ a/public/cliente/empresas.php
@@ -29,11 +29,11 @@
     <nav class="unDisplayNav">
         <ul>
             <li id="profile"><i class="fa fa-cogs fa-4x" id="iconProfile"></i><br><span id="titleProfile"><?php  echo $perfil['empresa'] ?></span><br><span id="nameProfile"><?php echo $perfil['correo']; ?></span></li>
-            <li><a href="#"><i class="fa fa-tachometer icons"></i>Dashboard</a></li>
-            <li><a href="#"><i class="fa fa-industry icons"></i>Empresas</a></li>
-            <li><a href="#"><i class="fa fa-bar-chart icons"></i>Históricos</a></li>
-            <li><a href="#"><i class="fa fa-send icons"></i>Contácto</a></li>
-            <li><a href="#"><i class="fa fa-unlock icons"></i>Contraseña</a></li>
+            <li><a><i class="fa fa-tachometer icons"></i>Dashboard</a></li>
+            <li><a><i class="fa fa-industry icons"></i>Empresas</a></li>
+            <li><a><i class="fa fa-bar-chart icons"></i>Históricos</a></li>
+            <li><a><i class="fa fa-send icons"></i>Contácto</a></li>
+            <li><a><i class="fa fa-unlock icons"></i>Contraseña</a></li>
             <li><a href="#"><i class="fa fa-sign-out icons"></i>Cerrar</a></li>
         </ul>
     </nav>
@@ -45,117 +45,112 @@
                     <div class="dropdown fRight">
                         <div class="btn dropdown-toogle" style="background-color: white;" data-toggle="dropdown"><i class="fa fa-ellipsis-v"></i></div>
                         <ul class="dropdown-menu dropdown-menu-right">
-                            <li><a><i class="fa fa-pencil" id="'.$value['idEmpresa'].'"></i>editar</a></li>
-                            <li><a><i class="fa fa-remove" id="'.$value['zonas'].'"></i>remover</a></li>
+                            <li><a id="1" class="btnEditBusiness"><i class="fa fa-pencil" id="'.$value['idEmpresa'].'"></i>editar</a></li>
+                            <li><a id="1" class="btnRemoveBusiness"><i class="fa fa-remove" id="'.$value['zonas'].'"></i>remover</a></li>
                         </ul>
                     </div>
                     <span>'.$value['nombre'].'</span>
                 </div>
                 <div class="col-xs-4 tCenter"><i class="fa fa-map fa-2x"></i><br><span>ZONAS</span><br>'.$value['zonas'].'</div>
                 <div class="col-xs-4 tCenter"><i class="fa fa-truck fa-2x"></i><br><span>MÁQUINAS</span><br>'.$value['maquinas'].'</div>
-                <div class="col-xs-4 tCenter"><i class="fa fa-users  fa-2x"></i><br><span>SUPERVISORES</span><br>Juan Pérez</div>
-            </div>
-        </div>
-        <div class="col-xs-12 col-sm-6 card">
-            <div class="col-xs-12 shadow">
-                <div class="col-xs-12 titleIndustry">
-                    <i class="fa fa-industry fa-2x fLeft cA"></i>
-                    <div class="dropdown fRight">
-                        <div class="divHover" data-toggle="dropdown"><i class="fa fa-ellipsis-v"></i></div>
-                        <ul class="dropdown-menu dropdown-menu-right">
-                            <li><a><i class="fa fa-pencil" id="'.$value['idEmpresa'].'"></i>editar</a></li>
-                            <li><a><i class="fa fa-remove" id="'.$value['zonas'].'"></i>remover</a></li>
-                        </ul>
-                    </div>
-                    <span>'.$value['nombre'].'</span>
-                </div>
-                <div class="col-xs-4 tCenter"><i class="fa fa-map fa-2x"></i><br><span>ZONAS</span><br>'.$value['zonas'].'</div>
-                <div class="col-xs-4 tCenter"><i class="fa fa-truck fa-2x"></i><br><span>MÁQUINAS</span><br>'.$value['maquinas'].'</div>
-                <div class="col-xs-4 tCenter"><i class="fa fa-users  fa-2x"></i><br><span>SUPERVISORES</span><br>Juan Pérez</div>
-            </div>
-        </div>
-        <div class="col-xs-12 col-sm-6 card">
-            <div class="col-xs-12 shadow">
-                <div class="col-xs-12 titleIndustry">
-                    <i class="fa fa-industry fa-2x fLeft cA"></i>
-                    <div class="dropdown fRight">
-                        <div class="divHover" data-toggle="dropdown"><i class="fa fa-ellipsis-v"></i></div>
-                        <ul class="dropdown-menu dropdown-menu-right">
-                            <li><a><i class="fa fa-pencil" id="'.$value['idEmpresa'].'"></i>editar</a></li>
-                            <li><a><i class="fa fa-remove" id="'.$value['zonas'].'"></i>remover</a></li>
-                        </ul>
-                    </div>
-                    <span>'.$value['nombre'].'</span>
-                </div>
-                <div class="col-xs-4 tCenter"><i class="fa fa-map fa-2x"></i><br><span>ZONAS</span><br>'.$value['zonas'].'</div>
-                <div class="col-xs-4 tCenter"><i class="fa fa-truck fa-2x"></i><br><span>MÁQUINAS</span><br>'.$value['maquinas'].'</div>
-                <div class="col-xs-4 tCenter"><i class="fa fa-users  fa-2x"></i><br><span>SUPERVISORES</span><br>Juan Pérez</div>
-            </div>
-        </div>
-        <div class="col-xs-12 col-sm-6 card">
-            <div class="col-xs-12 shadow">
-                <div class="col-xs-12 titleIndustry">
-                    <i class="fa fa-industry fa-2x fLeft cA"></i>
-                    <div class="dropdown fRight">
-                        <div class="divHover" data-toggle="dropdown"><i class="fa fa-ellipsis-v"></i></div>
-                        <ul class="dropdown-menu dropdown-menu-right">
-                            <li><a><i class="fa fa-pencil" id="'.$value['idEmpresa'].'"></i>editar</a></li>
-                            <li><a><i class="fa fa-remove" id="'.$value['zonas'].'"></i>remover</a></li>
-                        </ul>
-                    </div>
-                    <span>'.$value['nombre'].'</span>
-                </div>
-                <div class="col-xs-4 tCenter"><i class="fa fa-map fa-2x"></i><br><span>ZONAS</span><br>'.$value['zonas'].'</div>
-                <div class="col-xs-4 tCenter"><i class="fa fa-truck fa-2x"></i><br><span>MÁQUINAS</span><br>'.$value['maquinas'].'</div>
-                <div class="col-xs-4 tCenter"><i class="fa fa-users  fa-2x"></i><br><span>SUPERVISORES</span><br>Juan Pérez</div>
+                <div class="col-xs-4 tCenter"><i class="fa fa-users  fa-2x"></i><br><span>SUPERVISORES</span><br>'.$value['supervisores'].'</div>
             </div>
         </div>
     </div>
     <div id="stickyButton"><i class="fa fa-plus"></i></div>
-<!--<div class="modal fade" tabindex="-1" role="dialog" aria-labelledby="gridSystemModalLabel">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-        <h4 class="modal-title" id="gridSystemModalLabel">Modal title</h4>
-      </div>
-      <div class="modal-body">
-        <div class="row">
-          <div class="col-md-4">.col-md-4</div>
-          <div class="col-md-4 col-md-offset-4">.col-md-4 .col-md-offset-4</div>
-        </div>
-        <div class="row">
-          <div class="col-md-3 col-md-offset-3">.col-md-3 .col-md-offset-3</div>
-          <div class="col-md-2 col-md-offset-4">.col-md-2 .col-md-offset-4</div>
-        </div>
-        <div class="row">
-          <div class="col-md-6 col-md-offset-3">.col-md-6 .col-md-offset-3</div>
-        </div>
-        <div class="row">
-          <div class="col-sm-9">
-            Level 1: .col-sm-9
-            <div class="row">
-              <div class="col-xs-8 col-sm-6">
-                Level 2: .col-xs-8 .col-sm-6
-              </div>
-              <div class="col-xs-4 col-sm-6">
-                Level 2: .col-xs-4 .col-sm-6
-              </div>
+<!-- MODALS -->
+    <!-- MODAL EDIT BUSINESS -->
+    <div id="modalEditBusiness" class="modal fade" data-backdrop="static" data-keyboard="false">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <i class="fa fa-industry"></i>
+                    <h3>Editar Empresa</h3>
+                </div>
+                <div class="modal-body">
+                    <div class="row">
+                        <div class="col-md-4">.col-md-4</div>
+                        <div class="col-md-4 col-md-offset-4">.col-md-4 .col-md-offset-4</div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-3 col-md-offset-3">.col-md-3 .col-md-offset-3</div>
+                        <div class="col-md-2 col-md-offset-4">.col-md-2 .col-md-offset-4</div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-6 col-md-offset-3">.col-md-6 .col-md-offset-3</div>
+                    </div>
+                    <div class="row">
+                        <div class="col-sm-9">
+                            Level 1: .col-sm-9
+                            <div class="row">
+                                <div class="col-xs-8 col-sm-6">
+                                    Level 2: .col-xs-8 .col-sm-6
+                                </div>
+                                <div class="col-xs-4 col-sm-6">
+                                    Level 2: .col-xs-4 .col-sm-6
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Save changes</button>
+                </div>
             </div>
-          </div>
         </div>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
-      </div>
     </div>
-  </div
-</div>-->
+    <!-- MODAL REMOVE BUSINESS -->
+    <div id="modalRemoveBusiness" class="modal fade" aria-labelledby="gridSystemModalLabel">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                    <h4 class="modal-title" id="gridSystemModalLabel">REMOVER EMPRESA</h4>
+                </div>
+                <div class="modal-body">
+                    <div class="row">
+                        <div class="col-md-4">.col-md-4</div>
+                        <div class="col-md-4 col-md-offset-4">.col-md-4 .col-md-offset-4</div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-3 col-md-offset-3">.col-md-3 .col-md-offset-3</div>
+                        <div class="col-md-2 col-md-offset-4">.col-md-2 .col-md-offset-4</div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-6 col-md-offset-3">.col-md-6 .col-md-offset-3</div>
+                    </div>
+                    <div class="row">
+                        <div class="col-sm-9">
+                            Level 1: .col-sm-9
+                            <div class="row">
+                                <div class="col-xs-8 col-sm-6">
+                                    Level 2: .col-xs-8 .col-sm-6
+                                </div>
+                                <div class="col-xs-4 col-sm-6">
+                                    Level 2: .col-xs-4 .col-sm-6
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Save changes</button>
+                </div>
+            </div>
+        </div>
+    </div>
+<!-- MODALS -->
     <script src="../../js/jquery.js"></script>
     <script src="../../js/bootstrap.js"></script>
     <script src="../../js/funciones.js"></script>
-    <script>$(document).ready(main);</script>
-    <script>$('#modal')</script>
+    <script>
+        $(document).ready(function(){
+          	$('#modalEditBusiness').modal();
+            main();
+            modals();
+        });
+    </script>
 </body>
 </html>
