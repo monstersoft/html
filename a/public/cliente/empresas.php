d01cec5f4ddbacc0f60e04c04d816ac443991011
monstersoft <pavillanueva@ing.ucsc.cl>
2017-03-15 05:34:15 -0700

    j

--- a/public/cliente/empresas.php
+++ a/public/cliente/empresas.php
@@ -36,7 +36,19 @@
             <li><a><i class="fa fa-sign-out icons"></i>Cerrar</a></li>
         </ul>
     </nav>
-    <div id="content" class="unLeftContent"></div>
+    <div id="content" class="unLeftContent">
+        <?php 
+            if($empresas['cantidadEmpresas'] == 0)
+                echo 'No existen empresas, para agregar debes presionar el boton mas';
+            else {
+                foreach ($empresas['empresas'] as $key => $value) {
+                    echo '
+                        <div class="col-xs-12 col-sm-6 card"> <div class="col-xs-12 shadow"> <div class="col-xs-12 titleIndustry"> <i class="fa fa-industry fa-2x fLeft cA"></i> <div class="dropdown fRight"> <div class="btn dropdown-toogle" style="background-color: white;" data-toggle="dropdown"><i class="fa fa-ellipsis-v"></i></div><ul class="dropdown-menu dropdown-menu-right"> <li><a id="'.$value['idEmpresa'].'" class="editarEmpresa"><i class="fa fa-pencil"></i>editar</a></li><li><a id="'.$value['idEmpresa'].'" class="eliminarEmpresa"><i class="fa fa-remove"></i>remover</a></li></ul> </div><span>'.$value['nombre'].'</span> </div><div class="col-xs-4 tCenter"><i class="fa fa-map fa-2x"></i><br><span>ZONAS</span><br>'.$value['zonas'].'</div><div class="col-xs-4 tCenter"><i class="fa fa-truck fa-2x"></i><br><span>MÁQUINAS</span><br>'.$value['maquinas'].'</div><div class="col-xs-4 tCenter"><i class="fa fa-users fa-2x"></i><br><span>SUPERVISORES</span><br>'.$value['supervisores'].'</div></div><a href="zonas.php?id='.$value['idEmpresa'].'" class="boton">Ver</a> </div>
+                    ';
+                }
+            }
+        ?>
+    </div>
     <div id="stickyButton" class="agregarEmpresa"><i class="fa fa-plus"></i></div>
     <i id="loader" class="fa fa-circle-o-notch fa-spin fa-3x fa-fw"></i>
 <!-- VENTANAS MODALES -->
@@ -127,6 +139,7 @@
     <script src="../../cliente/js/modalEditarEmpresa.js"></script>
     <script>
         $(document).ready(function(){
+            $('#loader').addClass('loaderDisplayNav');
             main();
             $('.cancelar').click(function(){$('.alert').remove();});
             $('.modal').on('hidden.bs.modal', function(){
@@ -134,5 +147,6 @@
             });
         });
     </script>
+    <script type="text/javascript">$(window).load(function(){$('#loader').css('display','none')});</script>
 </body>
 </html>
