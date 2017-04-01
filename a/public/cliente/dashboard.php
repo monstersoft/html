709d0d88b17d61dcaa106fcb2c8496355b0667c3
monstersoft <pavillanueva@ing.ucsc.cl>
2017-04-01 01:08:38 -0300

    C

---
+++ a/public/cliente/dashboard.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <link rel="stylesheet" href="../../recursos/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../../recursos/awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="../../recursos/animate/animate.css">
+    <link rel="stylesheet" href="../../recursos/select2/select2.min.css">
+    <link rel="stylesheet" href="../../recursos/select2/select2-bootstrap.css">
+    <link rel="stylesheet" href="../../recursos/responsiveTables/responsiveTables.css">
+    <link rel="stylesheet" href="../../css/base.css">
+</head>
+<body>
+    <div id="bar"><a id="clickMenu"><i class="fa fa-bars"></i></a><p class="editarZona">Machine Monitors</p></div>
+    <nav class="unDisplayNav">
+        <ul>
+            <li id="profile"><i class="fa fa-cogs fa-4x" id="iconProfile"></i><br><span id="titleProfile">Pato</span><br><span id="nameProfile">Arauco</span></li>
+            <li><a class="selected"><i class="fa fa-tachometer icons"></i>Dashboard</a></li>
+            <li><a><i class="fa fa-industry icons"></i>Empresas</a></li>
+            <li><a><i class="fa fa-bar-chart icons"></i>Históricos</a></li>
+            <li><a><i class="fa fa-send icons"></i>Contácto</a></li>
+            <li><a><i class="fa fa-unlock icons"></i>Contraseña</a></li>
+            <li><a><i class="fa fa-sign-out icons"></i>Cerrar</a></li>
+        </ul>
+    </nav>
+    <div id="content" class="animated fadeInUp unLeftContent">
+<!-- ............................................................................................................................ -->
+     
+<div class="col-xs-12 card">
+    <div class="col-xs-12 shadow cardContent">
+        <div class="col-xs-12 titleCard"> <i class="fa fa-line-chart pull-left"></i>
+            <div class="dropdown pull-right">
+                <div class="btn dropdown-toogle" style="background-color: white;" data-toggle="dropdown"><i class="fa fa-ellipsis-v"></i></div>
+                <ul class="dropdown-menu dropdown-menu-right">
+                    <li><a id="'.$value['idEmpresa'].'" class="editarEmpresa"><i class="fa fa-pencil"></i>editar</a></li>
+                    <li><a id="'.$value['idEmpresa'].'" class="eliminarEmpresa"><i class="fa fa-remove"></i>remover</a></li>
+                </ul>
+            </div>
+            <p>GRADOS PALA</p>
+        </div>
+        <div class="col-xs-12 cardContent" style="padding: 20px;">
+        <div style="width: 100%;">
+            <div id="parent">
+                <canvas style="height: 300px;" id="myChart"></canvas>
+            </div>
+        </div>
+        </div>
+    </div>
+</div>
+<!-- ............................................................................................................................ -->
+    </div>
+    <script src="../../recursos/jquery/jquery.min.js"></script>
+    <script src="../../recursos/bootstrap/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.js"></script>
+    <script src="../../recursos/select2/select2.full.js"></script>
+    <script src="../../recursos/responsiveTables/responsiveTables.js"></script>
+    <script src="../../js/funciones.js"></script>
+    <script>
+        $(document).ready(function(){
+            var desplegar = 0;
+            main();
+            $('.agregar').click(function(){
+                $('.sOne').toggleClass('displaySticky');
+                $('.sTwo').toggleClass('displaySticky');
+            });
+            $('.cancelar').click(function(){$('.alert').remove();});
+            $('.modal').on('hidden.bs.modal', function(){
+                $(this).find('form')[0].reset();
+                $("#zonasAsociadas").find("option[class='dinamico']").remove();
+            });
+        });
+    </script>
+    <script>
+        $.fn.select2.defaults.set( "theme", "bootstrap" );
+        $( ".select2-multiple" ).select2( {
+            placeholder: "Seleccionar",
+            width: null,
+            containerCssClass: ':all:'
+        } );
+        $( ".select2-multiple" ).on( "select2:open", function() {
+            if ( $( this ).parents( "[class*='has-']" ).length ) {
+                var classNames = $( this ).parents( "[class*='has-']" )[ 0 ].className.split( /\s+/ );
+                for ( var i = 0; i < classNames.length; ++i ) {
+                    if ( classNames[ i ].match( "has-" ) ) {
+                        $( "body > .select2-container" ).addClass( classNames[ i ] );
+                    }
+                }
+            }
+        });
+    </script>
+<script>
+        var canvas = document.getElementById("myChart");
+        var ctx = canvas.getContext("2d");
+
+        var parent = document.getElementById('parent');
+
+        canvas.width = parent.offsetWidth;
+        canvas.height = parent.offsetHeight;
+
+        var chart = new Chart(ctx, {
+          type: 'line',
+          options: {
+            responsive: true,
+            maintainAspectRatio: false,
+            scales: {
+                    xAxes: [{
+                        ticks: {
+                       autoSkip: true,
+                  maxRotation: 0,
+                  minRotation: 90,
+                fontSize: 12
+                        }
+                    }]
+                }
+          },
+          data: {
+            labels: ['1','2','3','4','5','6','7','8','9','10',
+            '11','12','13','14','15','16','17','18','19','20',
+            '21','22','23','24','25','26','27','28','29','30',
+            '31','32','33','34','35','36','37','38','39','40',
+            '41','42','43','44','45','46','47','48','49','50',
+            '51','52','53','54','55','56','57','58','59'],
+            datasets: [
+              {
+                label: 'Test 01',
+                data: [1, 2, 3, 4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,
+                21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,
+                43,44,45,46,47,48,49,50,51,54,53,55,55,56,57,58,59,60]
+              },
+              {
+                label: 'Test 03',
+                data: [3, 2, 5, 10, 23, 21]
+              }
+            ]
+          }
+        });
+    </script>
+</body>
+</html>
