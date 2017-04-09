327b25c9b75ffcd04ec2b6983fd3bd73af1d1cdb
monstersoft <pavillanueva@ing.ucsc.cl>
2017-04-09 04:48:49 -0300

    h

--- b.php
+++ b.php
@@ -31,62 +31,29 @@
     </style>
 </head>
 <body>
+<img id="scream" src="robot.jpg" alt="The Scream">
+<canvas id="myCanvas" height="549" width="300"></canvas>
 
-<div class="col">
-    <div class="c0"></div>
-    <div class="c1"><div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam a veritatis delectus perferendis consectetur ipsa vitae, earum odit quibusdam impedit soluta voluptas quis voluptatibus quia eius quisquam, esse at tempora!</div>
-    <div>Id, veniam? Unde voluptatum reiciendis nisi, libero quas accusantium omnis rerum atque nam deserunt error fugit voluptatibus ipsum, ipsam architecto deleniti inventore doloribus neque mollitia dolores ex recusandae sapiente saepe.</div>
-    <div>Atque repellat ab animi doloremque ad, molestiae quaerat iusto ea aspernatur consequatur deleniti necessitatibus aperiam voluptatum ipsum totam pariatur dolorem ratione dicta vel labore dolorum, modi similique ullam maiores. Quia!</div>
-    <div>Officiis iusto repellat, sapiente iste voluptatum est odio, tenetur! Itaque provident fuga, dolor maiores fugit consequatur inventore delectus eos assumenda, quo est ipsa dolorem eligendi voluptas porro quas labore aut.</div>
-    <div>Porro ipsa, placeat eligendi, incidunt labore harum eum doloribus explicabo, numquam obcaecati voluptate enim ullam blanditiis! At minus blanditiis consectetur, quam amet. Maiores nam ducimus porro. Repudiandae pariatur, eligendi sapiente?</div>
-    <div>Repellat autem sint incidunt cum est labore rem deleniti, aperiam sunt molestias libero, in reprehenderit ea soluta illum atque provident. Voluptates incidunt fuga quibusdam asperiores tempora nostrum, molestiae deserunt voluptate.</div>
-    <div>Iusto sequi nesciunt, laboriosam saepe qui maxime et, quasi fugit sit perferendis voluptates rerum amet sapiente a dolorem voluptas pariatur. Fugiat temporibus expedita eum autem corporis reprehenderit esse aut soluta!</div>
-    <div>Laborum itaque iusto omnis, corporis rerum doloribus ducimus iure voluptates libero reprehenderit voluptatibus voluptatum quaerat laudantium facere vero dicta, aspernatur. Expedita dicta eos alias voluptas praesentium quos corporis saepe provident!</div>
-    <div>Dicta adipisci sequi aliquam sit, quae inventore id quia quos repudiandae voluptas, recusandae facere accusamus labore, doloremque porro enim rerum eum nobis laboriosam consectetur repellendus provident? Nostrum qui eum dicta.</div>
-    <div>Quod praesentium consectetur nostrum quos. Quos dolores vitae, officiis repellat sed eum! Quidem quis, earum id nesciunt incidunt eveniet dolore quia voluptates in saepe animi maxime nemo perspiciatis adipisci quam.</div></div>
-</div>
-
-<div class="col">
-    <div class="c0"></div>
-    <div class="c1"><div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam a veritatis delectus perferendis consectetur ipsa vitae, earum odit quibusdam impedit soluta voluptas quis voluptatibus quia eius quisquam, esse at tempora!</div>
-    <div>Id, veniam? Unde voluptatum reiciendis nisi, libero quas accusantium omnis rerum atque nam deserunt error fugit voluptatibus ipsum, ipsam architecto deleniti inventore doloribus neque mollitia dolores ex recusandae sapiente saepe.</div>
-    <div>Atque repellat ab animi doloremque ad, molestiae quaerat iusto ea aspernatur consequatur deleniti necessitatibus aperiam voluptatum ipsum totam pariatur dolorem ratione dicta vel labore dolorum, modi similique ullam maiores. Quia!</div>
-    <div>Officiis iusto repellat, sapiente iste voluptatum est odio, tenetur! Itaque provident fuga, dolor maiores fugit consequatur inventore delectus eos assumenda, quo est ipsa dolorem eligendi voluptas porro quas labore aut.</div>
-    <div>Porro ipsa, placeat eligendi, incidunt labore harum eum doloribus explicabo, numquam obcaecati voluptate enim ullam blanditiis! At minus blanditiis consectetur, quam amet. Maiores nam ducimus porro. Repudiandae pariatur, eligendi sapiente?</div>
-    <div>Repellat autem sint incidunt cum est labore rem deleniti, aperiam sunt molestias libero, in reprehenderit ea soluta illum atque provident. Voluptates incidunt fuga quibusdam asperiores tempora nostrum, molestiae deserunt voluptate.</div>
-    <div>Iusto sequi nesciunt, laboriosam saepe qui maxime et, quasi fugit sit perferendis voluptates rerum amet sapiente a dolorem voluptas pariatur. Fugiat temporibus expedita eum autem corporis reprehenderit esse aut soluta!</div>
-    <div>Laborum itaque iusto omnis, corporis rerum doloribus ducimus iure voluptates libero reprehenderit voluptatibus voluptatum quaerat laudantium facere vero dicta, aspernatur. Expedita dicta eos alias voluptas praesentium quos corporis saepe provident!</div>
-    <div>Dicta adipisci sequi aliquam sit, quae inventore id quia quos repudiandae voluptas, recusandae facere accusamus labore, doloremque porro enim rerum eum nobis laboriosam consectetur repellendus provident? Nostrum qui eum dicta.</div>
-    <div>Quod praesentium consectetur nostrum quos. Quos dolores vitae, officiis repellat sed eum! Quidem quis, earum id nesciunt incidunt eveniet dolore quia voluptates in saepe animi maxime nemo perspiciatis adipisci quam.</div></div>
-</div>
-
-<div class="col">
-    <div class="c0"></div>
-    <div class="c1"><div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam a veritatis delectus perferendis consectetur ipsa vitae, earum odit quibusdam impedit soluta voluptas quis voluptatibus quia eius quisquam, esse at tempora!</div>
-    <div>Id, veniam? Unde voluptatum reiciendis nisi, libero quas accusantium omnis rerum atque nam deserunt error fugit voluptatibus ipsum, ipsam architecto deleniti inventore doloribus neque mollitia dolores ex recusandae sapiente saepe.</div>
-    <div>Atque repellat ab animi doloremque ad, molestiae quaerat iusto ea aspernatur consequatur deleniti necessitatibus aperiam voluptatum ipsum totam pariatur dolorem ratione dicta vel labore dolorum, modi similique ullam maiores. Quia!</div>
-    <div>Officiis iusto repellat, sapiente iste voluptatum est odio, tenetur! Itaque provident fuga, dolor maiores fugit consequatur inventore delectus eos assumenda, quo est ipsa dolorem eligendi voluptas porro quas labore aut.</div>
-    <div>Porro ipsa, placeat eligendi, incidunt labore harum eum doloribus explicabo, numquam obcaecati voluptate enim ullam blanditiis! At minus blanditiis consectetur, quam amet. Maiores nam ducimus porro. Repudiandae pariatur, eligendi sapiente?</div>
-    <div>Repellat autem sint incidunt cum est labore rem deleniti, aperiam sunt molestias libero, in reprehenderit ea soluta illum atque provident. Voluptates incidunt fuga quibusdam asperiores tempora nostrum, molestiae deserunt voluptate.</div>
-    <div>Iusto sequi nesciunt, laboriosam saepe qui maxime et, quasi fugit sit perferendis voluptates rerum amet sapiente a dolorem voluptas pariatur. Fugiat temporibus expedita eum autem corporis reprehenderit esse aut soluta!</div>
-    <div>Laborum itaque iusto omnis, corporis rerum doloribus ducimus iure voluptates libero reprehenderit voluptatibus voluptatum quaerat laudantium facere vero dicta, aspernatur. Expedita dicta eos alias voluptas praesentium quos corporis saepe provident!</div>
-    <div>Dicta adipisci sequi aliquam sit, quae inventore id quia quos repudiandae voluptas, recusandae facere accusamus labore, doloremque porro enim rerum eum nobis laboriosam consectetur repellendus provident? Nostrum qui eum dicta.</div>
-    <div>Quod praesentium consectetur nostrum quos. Quos dolores vitae, officiis repellat sed eum! Quidem quis, earum id nesciunt incidunt eveniet dolore quia voluptates in saepe animi maxime nemo perspiciatis adipisci quam.</div></div>
-</div>
-
-<div class="col">
-    <div class="c0"></div>
-    <div class="c1"><div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam a veritatis delectus perferendis consectetur ipsa vitae, earum odit quibusdam impedit soluta voluptas quis voluptatibus quia eius quisquam, esse at tempora!</div>
-    <div>Id, veniam? Unde voluptatum reiciendis nisi, libero quas accusantium omnis rerum atque nam deserunt error fugit voluptatibus ipsum, ipsam architecto deleniti inventore doloribus neque mollitia dolores ex recusandae sapiente saepe.</div>
-    <div>Atque repellat ab animi doloremque ad, molestiae quaerat iusto ea aspernatur consequatur deleniti necessitatibus aperiam voluptatum ipsum totam pariatur dolorem ratione dicta vel labore dolorum, modi similique ullam maiores. Quia!</div>
-    <div>Officiis iusto repellat, sapiente iste voluptatum est odio, tenetur! Itaque provident fuga, dolor maiores fugit consequatur inventore delectus eos assumenda, quo est ipsa dolorem eligendi voluptas porro quas labore aut.</div>
-    <div>Porro ipsa, placeat eligendi, incidunt labore harum eum doloribus explicabo, numquam obcaecati voluptate enim ullam blanditiis! At minus blanditiis consectetur, quam amet. Maiores nam ducimus porro. Repudiandae pariatur, eligendi sapiente?</div>
-    <div>Repellat autem sint incidunt cum est labore rem deleniti, aperiam sunt molestias libero, in reprehenderit ea soluta illum atque provident. Voluptates incidunt fuga quibusdam asperiores tempora nostrum, molestiae deserunt voluptate.</div>
-    <div>Iusto sequi nesciunt, laboriosam saepe qui maxime et, quasi fugit sit perferendis voluptates rerum amet sapiente a dolorem voluptas pariatur. Fugiat temporibus expedita eum autem corporis reprehenderit esse aut soluta!</div>
-    <div>Laborum itaque iusto omnis, corporis rerum doloribus ducimus iure voluptates libero reprehenderit voluptatibus voluptatum quaerat laudantium facere vero dicta, aspernatur. Expedita dicta eos alias voluptas praesentium quos corporis saepe provident!</div>
-    <div>Dicta adipisci sequi aliquam sit, quae inventore id quia quos repudiandae voluptas, recusandae facere accusamus labore, doloremque porro enim rerum eum nobis laboriosam consectetur repellendus provident? Nostrum qui eum dicta.</div>
-    <div>Quod praesentium consectetur nostrum quos. Quos dolores vitae, officiis repellat sed eum! Quidem quis, earum id nesciunt incidunt eveniet dolore quia voluptates in saepe animi maxime nemo perspiciatis adipisci quam.</div></div>
-</div>
+<script>
+window.onload = function() {
+    var c = document.getElementById("myCanvas");
+    var ctx = c.getContext("2d");
+    var img = document.getElementById("scream");
+    
+    /*drawImage(
+        Specifies the image canvas or video element to use, 
+        The x coordinate where to start clipping, 
+        The y coordinate where to start clipping,
+        The width of the clipped image,
+        The height of the clipped image,
+        The x coordinate where to place the image on the canvas,
+        The y coordinate where to place the image on the canvas,
+        The width of the image to use,
+        The height of the image to use)*/
+    
+    ctx.drawImage(img,0,0,300,549,0,0,300,549);
+};
+</script>
 
 </body>
 </html
