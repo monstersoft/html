8b7ad627f95893d2a0dac7eb3c21006a882deb12
monstersoft <pavillanueva@ing.ucsc.cl>
2017-04-10 08:59:57 -0300

    h

--- a/cliente/js/graficos.js
+++ a/cliente/js/graficos.js
@@ -1,23 +1,79 @@
 var data = {
-    labels: ['08:00','08:01','08:02','08:03','08:04','08:05','08:06','08:07','08:08','08:09',
-                '08:10','08:11','08:12','08:13','08:14','08:15','08:16','08:17','08:18','08:19',
-                '08:20','08:21','08:22','08:23','08:24','08:25','08:26','08:27','08:28','08:28',
-                '08:30','08:31','08:32','08:33','08:34','08:35','08:36','08:37','08:38','08:39',
-                '08:40','08:41','08:42','08:43','08:44','08:45','08:46','08:47','08:48','08:49',
-                '08:50','08:51','08:52','08:53','08:54','08:55','08:56','08:57','08:58','08:59'],
+    labels: [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,
+              31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59],
     series: [
-        get(0,3600),
-        get(0,3600)
+        get(0,3600,59),
+        get(0,3600,59)
     ]
     };
 graphedChartLine('#chartLineSticky', true, data);
 graphedChartLine('#chartLine', false, data);
-graphedChartLine('#example', false, data);
+
+var data = {
+  labels: ['Bananas', 'Apples', 'Grapes'],
+  series: [20, 15, 40]
+};
+
+var options = {
+  labelInterpolationFnc: function(value) {
+    return value[0]
+  }
+};
+
+var responsiveOptions = [
+  ['screen and (min-width: 640px)', {
+    chartPadding: 5,
+    labelOffset: 10,
+    labelDirection: 'explode',
+    labelInterpolationFnc: function(value) {
+      return value;
+    }
+  }],
+  ['screen and (min-width: 1024px)', {
+    labelOffset: 100,
+    chartPadding: 0
+  }]
+];
+
+new Chartist.Pie('#example', data, options, responsiveOptions);
+
+
+
+var data = {
+  series: [5, 3, 4]
+};
+
+var sum = function(a, b) { return a + b };
+
+new Chartist.Pie('#example2', data, {
+  labelInterpolationFnc: function(value) {
+    return Math.round(value / data.series.reduce(sum) * 100) + '%';
+  }
+});
+
+new Chartist.Pie('#example3', {
+  series: [20, 10, 30, 40]
+}, {
+  donut: true,
+  donutWidth: 60,
+  startAngle: 270,
+  total: 200,
+  showLabel: false
+});
+
+
+
 
 function graphedChartLine(idChart, axisShowY, data) {
     var options = {    
         axisY: {
           showLabel: axisShowY,
+          labelInterpolationFnc: function(value) {
+              return value + '°';
+          }
+        },
+        axisX: {
+          labelInterpolationFnc: function(value){return value+"'"}
         },
         plugins: [
             Chartist.plugins.tooltip()
@@ -25,10 +81,21 @@ function graphedChartLine(idChart, axisShowY, data) {
     }
     new Chartist.Line(idChart, data, options);
 }
-function get(min, max) {
+function graphedChartDonut(idChart){
+    new Chartist.Pie(idChart, {
+  series: get(1,10,10)
+}, {
+  donut: true,
+  donutWidth: 60,
+  startAngle: 270,
+  total: 10,
+  showLabel: false
+});
+}
+function get(min, max, cantidad) {
   var a = [];
-  var i=0;
-  while(i<=60) {
+  var i=1;
+  while(i<=cantidad) {
       a.push(Math.floor(Math.random() * (max - min + 1)) + min);
       i++;
   }
--- index.php
+++ index.php
@@ -10,11 +10,20 @@
     <link rel="stylesheet" href="a/css/dashboard.css">
 </head>
     <body>
-        <div class="col-xs-12 well" style="background: rgba(0,0,0,0.1);">
+        <div class="col-xs-12">
             <div class="chartLineContainer">
                 <div class="chartLineSticky"><div id="chartLineSticky"></div><div class="chartLineBackground"></div></div>
                 <div class="chartLine"><div id="chartLine"></div></div>
             </div>
+        </div>
+        <div class="col-xs-12 col-sm-6">
+            <div id="example" style="height: 250px;"></div>
+        </div>
+        <div class="col-xs-12 col-sm-6">
+            <div id="example2" style="height: 250px;"></div>
+        </div>
+        <div class="col-xs-12 col-sm-6">
+            <div id="example3" style="height: 250px;"></div>
         </div>
         <script src="a/recursos/jquery/jquery.min.js"></script>
         <script src="a/recursos/bootstrap/js/bootstrap.min.js"></script>
