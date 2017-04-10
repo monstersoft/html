fb12ab050ef32467f0d75fb938829ee76ee4a459
monstersoft <pavillanueva@ing.ucsc.cl>
2017-04-10 07:04:23 -0300

    s

--- a/cliente/js/graficos.js
+++ a/cliente/js/graficos.js
@@ -1,302 +1,36 @@
-        var canvas = document.getElementById("myChart");
-        var ctx = canvas.getContext("2d");
-
-
-        var canvas2 = document.getElementById("myChart2");
-        var ctx2 = canvas2.getContext("2d");
-
-        var y = get(0,60);
-        var chart2 = new Chart(ctx2, {
-            type: 'line',
-            options: {
-                legend: {
-                    display: true,
-                },
-                responsive: true,
-                maintainAspectRatio: false,
-                scales: {
-                    xAxes: [{
-                        gridLines: {
-                            display: true
-                        },
-                        ticks: {
-                        autoSkip: true,
-                        maxRotation: 0,
-                        minRotation: 90,
-                        fontSize: 12,
-                        beginAtZero: true,
-                        /*maxTicksLimit: 20*/
-                        }
-                    }],
-                    yAxes:[{
-                        gridLines: {
-                            display: true
-                        },
-                        ticks: {
-                            beginAtZero: true,
-                            display: false
-                        },
-                        stacked: false
-                    }]
-                }
-            },
-            data: {
-                labels: ['08:00 am','08:01 am','08:02 am','08:03 am','08:04 am','08:05 am','08:06 am','08:07 am','08:08 am','08:09 am',
-                '08:10 am','08:11 am','08:12 am','08:13 am','08:14 am','08:15 am','08:16 am','08:17 am','08:18 am','08:19 am',
-                '08:20 am','08:21 am','08:22 am','08:23 am','08:24 am','08:25 am','08:26 am','08:27 am','08:28 am','08:28 am',
-                '08:30 am','08:31 am','08:32 am','08:33 am','08:34 am','08:35 am','08:36 am','08:37 am','08:38 am','08:39 am',
-                '08:40 am','08:41 am','08:42 am','08:43 am','08:44 am','08:45 am','08:46 am','08:47 am','08:48 am','08:49 am',
-                '08:50 am','08:51 am','08:52 am','08:53 am','08:54 am','08:55 am','08:56 am','08:57 am','08:58 am','08:59 am'],
-                datasets: [
-                {
-                    label: 'FRONTAL',
-                    fill: false,
-                    lineTension: 0,
-                    backgroundColor: "#F5A214",
-                    borderColor: "#F5A214",
-                    borderWidth: 1,
-                    pointRadius: 3,
-                    pointBorderWidth: 3,
-                    data: y
-                },
-                {
-                    label: 'TRASERA',
-                    fill: false,
-                    lineTension: 0,
-                    backgroundColor: "#262626",
-                    borderColor: "#262626",
-                    borderWidth: 1,
-                    pointRadius: 1,
-                    data: y
-                }
-                ]
-            }
-        });
-
-
-
-
-        var chart = new Chart(ctx, {
-            type: 'line',
-            options: {
-                legend: {
-                    display: true
-                },
-                responsive: true,
-                maintainAspectRatio: false,
-                scales: {
-                    xAxes: [{
-                        gridLines: {
-                            display: true
-                        },
-                        ticks: {
-                        autoSkip: true,
-                        maxRotation: 0,
-                        minRotation: 90,
-                        fontSize: 12,
-                        beginAtZero: true,
-                        //maxTicksLimit: 20
-                        }
-                    }],
-                    yAxes:[{
-                        gridLines: {
-                            display: true
-                        },
-                        ticks: {
-                            beginAtZero: true,
-                            //display: false
-                        },
-                        stacked: false
-                    }]
-                }
-            },
-            data: {
-                labels: ['08:00 am','08:01 am','08:02 am','08:03 am','08:04 am','08:05 am','08:06 am','08:07 am','08:08 am','08:09 am',
-                '08:10 am','08:11 am','08:12 am','08:13 am','08:14 am','08:15 am','08:16 am','08:17 am','08:18 am','08:19 am',
-                '08:20 am','08:21 am','08:22 am','08:23 am','08:24 am','08:25 am','08:26 am','08:27 am','08:28 am','08:28 am',
-                '08:30 am','08:31 am','08:32 am','08:33 am','08:34 am','08:35 am','08:36 am','08:37 am','08:38 am','08:39 am',
-                '08:40 am','08:41 am','08:42 am','08:43 am','08:44 am','08:45 am','08:46 am','08:47 am','08:48 am','08:49 am',
-                '08:50 am','08:51 am','08:52 am','08:53 am','08:54 am','08:55 am','08:56 am','08:57 am','08:58 am','08:59 am'],
-                datasets: [
-                {
-                    label: 'FRONTAL',
-                    fill: false,
-                    lineTension: 0,
-                    backgroundColor: "#F5A214",
-                    borderColor: "#F5A214",
-                    borderWidth: 1,
-                    pointRadius: 3,
-                    pointBorderWidth: 3,
-                    data: get(0,5000)
-                },
-                {
-                    label: 'TRASERA',
-                    fill: false,
-                    lineTension: 0,
-                    backgroundColor: "#262626",
-                    borderColor: "#262626",
-                    borderWidth: 1,
-                    pointRadius: 1,
-                    data: y
-                }
-                ]
-            }
-        });
-
-
-
-        function get(min, max) {
-          var a = [];
-          var i=0;
-          while(i<=60) {
-              a.push(Math.floor(Math.random() * (max - min + 1)) + min);
-              i++;
-          }
-          return a;
-        }
-
-/*Chart.defaults.doughnutLabels = Chart.helpers.clone(Chart.defaults.doughnut);
-
-var helpers = Chart.helpers;
-var defaults = Chart.defaults;
-
-Chart.controllers.doughnutLabels = Chart.controllers.doughnut.extend({
-	updateElement: function(arc, index, reset) {
-    var _this = this;
-    var chart = _this.chart,
-        chartArea = chart.chartArea,
-        opts = chart.options,
-        animationOpts = opts.animation,
-        arcOpts = opts.elements.arc,
-        centerX = (chartArea.left + chartArea.right) / 2,
-        centerY = (chartArea.top + chartArea.bottom) / 2,
-        startAngle = opts.rotation, // non reset case handled later
-        endAngle = opts.rotation, // non reset case handled later
-        dataset = _this.getDataset(),
-        circumference = reset && animationOpts.animateRotate ? 0 : arc.hidden ? 0 : _this.calculateCircumference(dataset.data[index]) * (opts.circumference / (2.0 * Math.PI)),
-        innerRadius = reset && animationOpts.animateScale ? 0 : _this.innerRadius,
-        outerRadius = reset && animationOpts.animateScale ? 0 : _this.outerRadius,
-        custom = arc.custom || {},
-        valueAtIndexOrDefault = helpers.getValueAtIndexOrDefault;
-
-    helpers.extend(arc, {
-      // Utility
-      _datasetIndex: _this.index,
-      _index: index,
-
-      // Desired view properties
-      _model: {
-        x: centerX + chart.offsetX,
-        y: centerY + chart.offsetY,
-        startAngle: startAngle,
-        endAngle: endAngle,
-        circumference: circumference,
-        outerRadius: outerRadius,
-        innerRadius: innerRadius,
-        label: valueAtIndexOrDefault(dataset.label, index, chart.data.labels[index])
-      },
-
-      draw: function () {
-      	var ctx = this._chart.ctx,
-						vm = this._view,
-						sA = vm.startAngle,
-						eA = vm.endAngle,
-						opts = this._chart.config.options;
-				
-					var labelPos = this.tooltipPosition();
-					var segmentLabel = vm.circumference / opts.circumference * 100;
-					
-					ctx.beginPath();
-					
-					ctx.arc(vm.x, vm.y, vm.outerRadius, sA, eA);
-					ctx.arc(vm.x, vm.y, vm.innerRadius, eA, sA, true);
-					
-					ctx.closePath();
-					ctx.strokeStyle = vm.borderColor;
-					ctx.lineWidth = vm.borderWidth;
-					
-					ctx.fillStyle = vm.backgroundColor;
-					
-					ctx.fill();
-					ctx.lineJoin = 'bevel';
-					
-					if (vm.borderWidth) {
-						ctx.stroke();
-					}
-					
-					if (vm.circumference > 0.15) { // Trying to hide label when it doesn't fit in segment
-						ctx.beginPath();
-						ctx.font = helpers.fontString(opts.defaultFontSize, opts.defaultFontStyle, opts.defaultFontFamily);
-						ctx.fillStyle = "#fff";
-						ctx.textBaseline = "top";
-						ctx.textAlign = "center";
-            
-            // Round percentage in a way that it always adds up to 100%
-						ctx.fillText(segmentLabel.toFixed(0) + "%", labelPos.x, labelPos.y);
-					}
-      }
-    });
-
-    var model = arc._model;
-    model.backgroundColor = custom.backgroundColor ? custom.backgroundColor : valueAtIndexOrDefault(dataset.backgroundColor, index, arcOpts.backgroundColor);
-    model.hoverBackgroundColor = custom.hoverBackgroundColor ? custom.hoverBackgroundColor : valueAtIndexOrDefault(dataset.hoverBackgroundColor, index, arcOpts.hoverBackgroundColor);
-    model.borderWidth = custom.borderWidth ? custom.borderWidth : valueAtIndexOrDefault(dataset.borderWidth, index, arcOpts.borderWidth);
-    model.borderColor = custom.borderColor ? custom.borderColor : valueAtIndexOrDefault(dataset.borderColor, index, arcOpts.borderColor);
-
-    // Set correct angles if not resetting
-    if (!reset || !animationOpts.animateRotate) {
-      if (index === 0) {
-        model.startAngle = opts.rotation;
-      } else {
-        model.startAngle = _this.getMeta().data[index - 1]._model.endAngle;
-      }
-
-      model.endAngle = model.startAngle + model.circumference;
-    }
-
-    arc.pivot();
-  }
-});
-
-var config = {
-  type: 'doughnutLabels',
-  data: {
-    datasets: [{
-      data: [
-        1200,
-        1112,
-        533,
-        202,
-        105,
-      ],
-      backgroundColor: [
-        "#F7464A",
-        "#46BFBD",
-        "#FDB45C",
-        "#949FB1",
-        "#4D5360",
-      ],
-      label: 'Dataset 1'
-    }],
-    labels: [
-      "Red",
-      "Green",
-      "Yellow",
-      "Grey",
-      "Dark Grey"
+var data = {
+    labels: ['08:00','08:01','08:02','08:03','08:04','08:05','08:06','08:07','08:08','08:09',
+                '08:10','08:11','08:12','08:13','08:14','08:15','08:16','08:17','08:18','08:19',
+                '08:20','08:21','08:22','08:23','08:24','08:25','08:26','08:27','08:28','08:28',
+                '08:30','08:31','08:32','08:33','08:34','08:35','08:36','08:37','08:38','08:39',
+                '08:40','08:41','08:42','08:43','08:44','08:45','08:46','08:47','08:48','08:49',
+                '08:50','08:51','08:52','08:53','08:54','08:55','08:56','08:57','08:58','08:59'],
+    series: [
+        get(0,3600),
+        get(0,3600)
     ]
-  },
-  options: {
-    responsive: true,
-    legend: {
-      position: 'bottom',
-    },
-    animation: {
-      animateScale: true,
-      animateRotate: true
-    }
-  }
-};
+    };
+graphedChartLine('#chartLineSticky', true, data);
+graphedChartLine('#chartLine', false, data);
+graphedChartLine('#example', false, data);
 
-var ctx = document.getElementById("myChart").getContext("2d");
-new Chart(ctx, config);*/
+function graphedChartLine(idChart, axisShowY, data) {
+    var options = {    
+        axisY: {
+          showLabel: axisShowY,
+        },
+        plugins: [
+            Chartist.plugins.tooltip()
+        ]
+    }
+    new Chartist.Line(idChart, data, options);
+}
+function get(min, max) {
+  var a = [];
+  var i=0;
+  while(i<=60) {
+      a.push(Math.floor(Math.random() * (max - min + 1)) + min);
+      i++;
+  }
+  return a;
+}
--- index.php
+++ index.php
@@ -7,129 +7,19 @@
     <link rel="stylesheet" href="a/recursos/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="a/recursos/chartist/chartist.min.css" />
     <link rel="stylesheet" href="a/recursos/chartist/chartist-plugin-tooltip.css">
-    <style>
-        /*.ct-labels foreignObject span {
-            position: fixed;
-            top: 0;
-            left: 0;
-            z-index: 1000;
-        }*/
-        .ct-series-a .ct-line {
-            stroke : #F5A214;
-            stroke-width: 1px;
-        }
-        .ct-series-b .ct-line {
-            stroke : #262626;
-            stroke-width: 1px;
-        }
-        .ct-series-a .ct-point {
-            stroke: #F5A214;
-            stroke-width: 12px;
-            stroke-linecap: square;
-        }
-        .ct-series-b .ct-point {
-            stroke: #262626;
-            stroke-width: 12px;
-            stroke-linecap: circle;
-        }
-        .ct-chart .ct-grid {
-            /*stroke-dasharray: none;*/
-            stroke: grey;
-        }
-        .a {
-            position: relative;
-            top: 0;
-            left: 0;
-            overflow: auto;
-            width: 100%;
-            height: 300px;
-            z-index: 0;
-        }
-        .b {
-            position: sticky;
-            top: 0;
-            left: 0;
-            z-index: 1;
-        }
-        .white {
-            position: absolute;
-            top: 0;
-            left: 40px;
-            width: 2000px;
-            height: 250px;
-            z-index: 2;
-            background: white;
-        }
-        .c {
-            position: absolute;
-            top: 0;
-            left: 0;
-            z-index: 1000;
-        }
-        .ct-label.ct-vertical { font-size: 12px; color: #262626;}
-        .ct-label.ct-horizontal { font-size: 12px; position: relative; transform: rotate(90deg); transform-origin: left top; color: #262626;}
-    </style>
+    <link rel="stylesheet" href="a/css/dashboard.css">
 </head>
     <body>
-      <div class="a">
-        <div class="b"><div class="ct-chart"></div><div class="white"></div></div>
-        <div class="c"><div class="ct-chart2"></div>
-        
-        
-        
+        <div class="col-xs-12 well" style="background: rgba(0,0,0,0.2);">
+            <div class="chartLineContainer">
+                <div class="chartLineSticky"><div id="chartLineSticky"></div><div class="chartLineBackground"></div></div>
+                <div class="chartLine"><div id="chartLine"></div></div>
+            </div>
         </div>
-       </div>
         <script src="a/recursos/jquery/jquery.min.js"></script>
         <script src="a/recursos/bootstrap/js/bootstrap.min.js"></script>
         <script src="a/recursos/chartist/chartist.min.js"></script>
         <script src="a/recursos/chartist/chartist-plugin-tooltip.js"></script>
-
-        <script>
-            $(document).ready(function(){
-                
-            });
-        </script>
-<script>
-var data = {
-    labels: ['08:00','08:01','08:02','08:03','08:04','08:05','08:06','08:07','08:08','08:09',
-                '08:10','08:11','08:12','08:13','08:14','08:15','08:16','08:17','08:18','08:19',
-                '08:20','08:21','08:22','08:23','08:24','08:25','08:26','08:27','08:28','08:28',
-                '08:30','08:31','08:32','08:33','08:34','08:35','08:36','08:37','08:38','08:39',
-                '08:40','08:41','08:42','08:43','08:44','08:45','08:46','08:47','08:48','08:49',
-                '08:50','08:51','08:52','08:53','08:54','08:55','08:56','08:57','08:58','08:59'],
-    series: [
-        get(0,3600),
-        get(0,3600)
-    ]
-    };
-var options = {
-    width: 2000,
-    height: 250,
-    plugins: [
-        Chartist.plugins.tooltip()
-    ]
-}
-var options2 = {
-    width: 2000,
-    height: 250,
-    axisY: {
-      showLabel: false,
-    },
-    plugins: [
-        Chartist.plugins.tooltip()
-    ]
-}
-new Chartist.Line('.ct-chart2', data, options2);
-new Chartist.Line('.ct-chart', data, options);
-function get(min, max) {
-  var a = [];
-  var i=0;
-  while(i<=60) {
-      a.push(Math.floor(Math.random() * (max - min + 1)) + min);
-      i++;
-  }
-  return a;
-}
-</script>
+        <script src="a/cliente/js/graficos.js"></script>
     </body>
 </html>
