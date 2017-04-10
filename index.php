fc41a934c85f5b85b750d2c83673d67a1bc4aa5e
monstersoft <pavillanueva@ing.ucsc.cl>
2017-04-09 18:03:52 -0700

    d

--- index.php
+++ index.php
@@ -6,47 +6,50 @@
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <link rel="stylesheet" href="a/recursos/bootstrap/css/bootstrap.min.css">
     <style>
-.chartLineContainer {
-    height: 267px;
-    overflow: auto;
-    position: relative;
-}
-.copyChart {
-    position: sticky;
-    top: 0;
-    left: 0;
-    width: 200px;
-    height: 250px;
-    z-index: 0;
-}
-.whiteBackground {
-    position: absolute;
-    top: 0;
-    left: 32px;
-    width: 200px;
-    height: 250px;
-    z-index: 1;
-    background: white;
-}
-
-.lineChart {
-    position: absolute;
-    top: 0;
-    left: 33px;
-    width: 2000px;
-    /*width: 100%;*/
-    height: 250px;
-    z-index: 1000;
-}
-        
-
+        .chartLineContainer {
+            height: 267px;
+            overflow: auto;
+            position: relative;
+        }
+        .chartLine {
+            position: absolute;
+            top: 0;
+            left: 0;
+            z-index: 0;
+        }
+        .stickyChartLine {
+            position: sticky;
+            top: 0;
+            left: 0;
+            width: 2000px;
+            height: 250px;
+            z-index: 0;  
+        }
+        .whiteBackground {
+            position: absolute;
+            top: 0;
+            left: 32px;
+            width: 200px;
+            height: 250px;
+            z-index: 1;
+            background: white;
+        }
     </style>
 </head>
     <body>
-        <div class="col-xs-12" style="padding: 10px;">
+        <div class="col-xs-12" style="padding: 0px;">
             <div class="chartLineContainer">
-               <div class="lineChart"><canvas width="2000" style="height: 250px;" id="myChart2"></canvas></div>
-                <div class="copyChart"><canvas width="2000" style="height: 250px;" id="myChart"></canvas><div class="whiteBackground"></div></div>
+               <div class="chartLine">
+                   <canvas width="2000" height="250" id="chartLine"></canvas>
+                </div>
+            </div>
+        </div>
+        <div class="col-xs-12" style="padding: 0px;">
+            <div class="chartLineContainer">
+               <div class="stickyChartLine">
+                   <canvas id="stickyChartLine"></canvas>
+                   <!--<div class="whiteBackground"></div>-->
+               </div>
             </div>
         </div>
         <script src="a/recursos/jquery/jquery.min.js"></script>
