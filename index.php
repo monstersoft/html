260414b590b0be565a11cab97e97308fc172c5b1
monstersoft <pavillanueva@ing.ucsc.cl>
2017-04-11 01:39:52 -0300

    f

--- index.php
+++ index.php
@@ -7,24 +7,68 @@
     <link rel="stylesheet" href="a/recursos/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="a/recursos/chartist/chartist.min.css" />
     <link rel="stylesheet" href="a/recursos/chartist/chartist-plugin-tooltip.css">
+    <link rel="stylesheet" href="a/recursos/awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="a/css/base.css">
     <link rel="stylesheet" href="a/css/dashboard.css">
 </head>
     <body>
-        <div class="col-xs-12">
-            <div class="chartLineContainer">
-                <div class="chartLineSticky"><div id="chartLineSticky"></div><div class="chartLineBackground"></div></div>
-                <div class="chartLine"><div id="chartLine"></div></div>
+       
+       
+       
+        <div class="col-xs-12 col-sm-6 shadow cardContent">
+            <div class="col-xs-12 titleCard"> <i class="fa fa-pie-chart pull-left"></i>
+                <div class="dropdown pull-right">
+                    <div class="btn dropdown-toogle" style="background-color: white;" data-toggle="dropdown"><i class="fa fa-ellipsis-v"></i></div>
+                    <ul class="dropdown-menu dropdown-menu-right">
+                        <li><a id="'.$value['idEmpresa'].'" class="editarEmpresa"><i class="fa fa-pencil"></i>editar</a></li>
+                        <li><a id="'.$value['idEmpresa'].'" class="eliminarEmpresa"><i class="fa fa-remove"></i>remover</a></li>
+                    </ul>
+                </div>
+                <p>Estado de Motor</p>
+            </div>
+            <div class="col-xs-12 cardContent" style="padding: 10px;">
+                <div id="example2" style="height: 200px;"></div>
             </div>
         </div>
-        <div class="col-xs-12 col-sm-6">
-            <div id="example" style="height: 250px;"></div>
+        
+        
+        
+        <div class="col-xs-12 col-sm-6 shadow cardContent">
+            <div class="col-xs-12 titleCard"> <i class="fa fa-pie-chart pull-left"></i>
+                <div class="dropdown pull-right">
+                    <div class="btn dropdown-toogle" style="background-color: white;" data-toggle="dropdown"><i class="fa fa-ellipsis-v"></i></div>
+                    <ul class="dropdown-menu dropdown-menu-right">
+                        <li><a id="'.$value['idEmpresa'].'" class="editarEmpresa"><i class="fa fa-pencil"></i>editar</a></li>
+                        <li><a id="'.$value['idEmpresa'].'" class="eliminarEmpresa"><i class="fa fa-remove"></i>remover</a></li>
+                    </ul>
+                </div>
+                <p>Frecuencia de Cambios</p>
+            </div>
+            <div class="col-xs-12 cardContent" style="padding: 10px;">
+                <div id="example3" style="height: 200px;"></div>
+            </div>
         </div>
-        <div class="col-xs-12 col-sm-6">
-            <div id="example2" style="height: 250px;"></div>
-        </div>
-        <div class="col-xs-12 col-sm-6">
-            <div id="example3" style="height: 250px;"></div>
-        </div>
+        
+        
+        
+        <div class="col-xs-12 shadow cardContent">
+            <div class="col-xs-12 titleCard"> <i class="fa fa-line-chart pull-left"></i>
+                <div class="dropdown pull-right">
+                    <div class="btn dropdown-toogle" style="background-color: white;" data-toggle="dropdown"><i class="fa fa-ellipsis-v"></i></div>
+                    <ul class="dropdown-menu dropdown-menu-right">
+                        <li><a id="'.$value['idEmpresa'].'" class="editarEmpresa"><i class="fa fa-pencil"></i>editar</a></li>
+                        <li><a id="'.$value['idEmpresa'].'" class="eliminarEmpresa"><i class="fa fa-remove"></i>remover</a></li>
+                    </ul>
+                </div>
+                <p>Grados Pala</p>
+            </div>
+            <div class="col-xs-12 cardContent" style="padding: 10px;">
+                <div class="chartLineContainer">
+                    <div class="chartLineSticky"><div id="chartLineSticky"></div><div class="chartLineBackground"></div></div>
+                    <div class="chartLine"><div id="chartLine"></div></div>
+                </div>
+            </div>
+         </div>
         <script src="a/recursos/jquery/jquery.min.js"></script>
         <script src="a/recursos/bootstrap/js/bootstrap.min.js"></script>
         <script src="a/recursos/chartist/chartist.min.js"></script>
