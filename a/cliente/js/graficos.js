8e81f8ba83666c3c911ffd4273f951751bcf1a07
monstersoft <pavillanueva@ing.ucsc.cl>
2017-04-11 06:02:22 -0300

    s

--- a/cliente/js/graficos.js
+++ a/cliente/js/graficos.js
@@ -12,48 +12,22 @@ graphedChartLine('#chartLine', false, data);
 graphedChartDonut('#example');
 graphedChartBar('#example2');
 function graphedChartBar(idChart) {
-new Chartist.Bar(idChart, {
-  labels: ['1ra', '2da', '3ra', '4ta','5ta','6ta','7ma','8va','9na','10ma'],
-  series: [
-    [5, 4, 3, 7,10,12,7,8,9,10]
-  ]
-}, {
-  // Default mobile configuration
-  stackBars: true,
-  axisX: {
-    labelInterpolationFnc: function(value) {
-      return value.split(/\s+/).map(function(word) {
-        return word[0];
-      }).join('');
+    var data = {
+      labels: ['1ra', '2da', '3ra', '4ta','5ta','6ta','7ma','8va','9na','10ma'],
+      series: [
+        [5, 4, 3, 7,10,12,7,8,9,10]
+      ]
     }
-  },
-  axisY: {
-    offset: 20
-  }
-}, [
-  // Options override for media > 400px
-  ['screen and (min-width: 400px)', {
-    reverseData: true,
-    horizontalBars: true,
-    axisX: {
-      labelInterpolationFnc: Chartist.noop
-    },
-    axisY: {
-      offset: 60
+    var options = {
+      stackBars: true,
+      axisX: {
+        offset: 50,
+      },
+      axisY: {
+        offset: 20
+      }
     }
-  }],
-  // Options override for media > 800px
-  ['screen and (min-width: 800px)', {
-    stackBars: false,
-    seriesBarDistance: 10
-  }],
-  // Options override for media > 1000px
-  ['screen and (min-width: 1000px)', {
-    reverseData: false,
-    horizontalBars: false,
-    seriesBarDistance: 15
-  }]
-]);
+    new Chartist.Bar(idChart,data,options);
 }
 function graphedChartDonut(idChart){
     var sum = function(a,b) { return a+b; }
