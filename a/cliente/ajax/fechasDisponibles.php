77c82d7c0cd3ca78e759d3b0dd82d65b9874a63d
monstersoft <pavillanueva@ing.ucsc.cl>
2017-04-30 20:27:02 -0300

    h

--- a/cliente/ajax/fechasDisponibles.php
+++ a/cliente/ajax/fechasDisponibles.php
@@ -1,10 +1,21 @@
 <?php
 	include '../../php/conexion.php'; 
-	$idZona = $_POST['idZona'];
-    $fechaDatos = $_POST['fechaDatos'];
-    $arr = array();
-    $arr['sadasd'] = $idZona.'/'.$fechaDatos;
-    $arr['fechas'] = array([2017,03,27]);
-    $arr['fechas'][] = [2017,03,26];
+	$idZone = $_POST['idZona'];
+    $arr = array('firstDayAvailable' => array(), 'lastDayAvailable' => array(), 'availableDays' => array());
+    $con = conectar();
+    $qry = "SELECT archivos.idArchivo, archivos.fechaDatos , archivos.idZona FROM archivos WHERE archivos.idZona = '$idZone' ORDER BY archivos.fechaDatos ASC";
+    if($res = mysqli_query($con, $qry)) {
+        while($row = mysqli_fetch_assoc($res)) {
+            array_push($arr['availableDays'],dateArray($row['fechaDatos']));
+        }
+        $arr['firstDayAvailable'] = reset($arr['availableDays']);
+        $arr['lastDayAvailable'] = end($arr['availableDays']);
+    }
     echo json_encode($arr);
+    function dateArray($date) {
+        $arrayDay = array();
+        list($year,$month,$day) = explode('-',$date);
+        $arrayDay = [$year,$month-1,$day];
+        return  $arrayDay;
+    }
 ?>
--- a/cliente/js/fechasDisponibles.js
+++ a/cliente/js/fechasDisponibles.js
@@ -1,26 +1,48 @@
-function returnDays(callbackData) {
-    var url = devuelveUrl('a/cliente/ajax/fechasDisponibles.php');
-    return $.ajax({
-        url: url,
-        type: 'POST',
-        data: {idZona: 20, fechaDatos : '27-03-03'},
-        dataType: 'json',
-        cache: false,
-        success: function(arreglo) {callbackData(arreglo)},
-        error: function(xhr) {console.log(xhr.responseText);}
-    });
-}
 $('.datepicker').pickadate({
     onOpen: function(){
         var currentCalendar = this;
-        returnDays(function(response){
-            currentCalendar.set({
-                min: [2017,03,05],
-                max: [2017,03,10],
-                disable: [[2017,03,07],[2017,03,09]]
-            });
-         });
-     },
+        var url = devuelveUrl('a/cliente/ajax/fechasDisponibles.php');
+        $.ajax({
+            url: url,
+            type: 'POST',
+            data: {idZona: this.get('id')},
+            dataType: 'json',
+            cache: false,
+            success: function(arr) {
+                currentCalendar.set({
+                    min: arr.firstDayAvailable,
+                    max: arr.lastDayAvailable,
+                    disable: arr.availableDays
+                });
+                console.log(JSON.stringify(arr));
+            },
+            error: function(xhr) {console.log(xhr.responseText);}
+        });
+    },
+    onClose: function() {
+        var currentCalendar = this;
+        var sD = this.get('select');
+        var url = devuelveUrl('a/cliente/ajax/datosArchivo.php');
+        $.ajax({
+            url: url,
+            type: 'POST',
+            data: {idZona: this.get('id'), fechaDatos: sD.year+'-'+sD.month+1+'-'+sD.date},
+            dataType: 'json',
+            cache: false,
+            success: function(arr) {
+                var currentInput = document.getElementById(currentCalendar.get('id')).parentNode.parentNode.parentNode;
+                var supervisor = $(currentInput.childNodes[7]);
+                var fechaSubida = $(currentInput.childNodes[9]);
+                var horaSubida = $(currentInput.childNodes[11]);
+                supervisor.html(arr.nombreSupervisor);
+                fechaSubida.html(arr.fechaSubida);
+                horaSubida.html(arr.horaSubida);
+                console.log(JSON.stringify(arr));
+            },
+            error: function(xhr) {console.log(xhr.responseText);}
+        });
+        $(document.activeElement).blur();
+    },
     monthsFull: ['enero', 'febrero', 'marzo', 'abril', 'mayo', 'junio', 'julio', 'agosto', 'septiembre', 'octubre', 'noviembre', 'diciembre'],
     monthsShort: ['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dic'],
     weekdaysFull: ['domingo', 'lunes', 'martes', 'miércoles', 'jueves', 'viernes', 'sábado'],
