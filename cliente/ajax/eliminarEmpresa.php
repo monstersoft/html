f7f51752e48b113e61d41f3350e7b132ac2fbb9b
monstersoft <pavillanueva@ing.ucsc.cl>
2017-06-04 05:28:06 -0400

    SCD

--- cliente/ajax/eliminarEmpresa.php
+++ cliente/ajax/eliminarEmpresa.php
@@ -1,9 +1,25 @@
 <?php
     include '../../php/conexion.php';
-    $idEmpresa = $_POST['idEmpresa'];
+    //$idEmpresa = $_POST['idEmpresa'];
+    $idEmpresa = 297;
     $conexion = conectar();
     $arreglo = array();
-    $consulta = 
+    $auxOk = array();
+    $aux = array();
+    $resultadoSupervisores = array();
+    $consulta = "SELECT supervisores.idSupervisor FROM supervisores LEFT JOIN supervisoreszonas ON supervisores.idSupervisor = supervisoreszonas.idSupervisor LEFT JOIN zonas ON supervisoreszonas.idZona = zonas.idZona LEFT JOIN empresas ON zonas.idEmpresa = empresas.idEmpresa WHERE empresas.idEmpresa = '$idEmpresa' GROUP BY supervisores.idSupervisor";
+    if($res = mysqli_query($conexion,$consulta)) {
+        while($row = mysqli_fetch_assoc($res)) {
+            $aux[] = $row['idSupervisor'];
+        }
+    }
+    foreach($aux as $value) {
+        if(mysqli_query($conexion,"DELETE FROM supervisores WHERE supervisores.idSupervisor = '$value'"));
+            $auxOK[] = 'OK';
+    }
+    $arreglo['auxOK']= $auxOK;
+    $arreglo['aux'] = $aux;
+    echo json_encode($arreglo);
 /*
 TODOS LOS SUPERVISORES DE UNA ZONA X
 SELECT zonas.nombre, supervisoreszonas.idZona, supervisoreszonas.idSupervisor, supervisores.nombreSupervisor
@@ -18,3 +34,5 @@ SI TIENE UNO, TAMBIEN BORRO EN SUPERVISORES
 SINO BORRO SOLO EN SUPERVISORES ZONAS
 */
 ?>
+
+
