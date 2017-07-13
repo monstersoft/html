31f344693fb41f93b88f205ad5c018c6291a8b15
monstersoft <pavillanueva@ing.ucsc.cl>
2017-07-13 01:04:47 -0400

    s

--- f.php
+++ f.php
@@ -1,19 +1,72 @@
-            <?php
-            foreach(zonas($email) as $value) {
-                $idZona = $value['idZona'];
-                echo '<div class="col-xs-12 col-sm-12 card"> <div class="col-xs-12 shadow cardContent"> <div class="col-xs-12 titleCard"> <i class="fa fa-globe pull-left"></i> <div class="dropdown pull-right"> <div class="btn dropdown-toogle" style="background-color: white;" data-toggle="dropdown"><i class="fa fa-ellipsis-v"></i></div><ul class="dropdown-menu dropdown-menu-right"> <li><a id="'.$value['idZona'].'" class="subirArchivo"><i class="fa fa-upload"></i>subir archivo</a></li><li><a id="'.$value['idZona'].'" class="agregarMaquina"><i class="fa fa-cog"></i>agregar máquina</a></li><li><a id="'.$value['idZona'].'" class="descargarId"><i class="fa fa-download"></i>descargar id</a></li></ul> </div><p>'.$value['nombre'].'</p></div>';
-                    if(cantidadMaquinas($idZona) == 0)
-                        echo '<div class="col-xs-12 emptyMessage"><i class="fa fa-exclamation-circle fa-2x pull-left"></i>No hay máquinas asociadas a esta zona</div>';
-                    else {
-                        echo '<div class="col-xs-12 cardContent"> <div class="table-responsive"> <table class="responsive" style="width: 100%;"> <thead> <tr><th>Patente</th> <th>Fecha de Registro</th> <th>Tara [kg]</th> <th>Carga Máxima [kg]</th> </tr></thead> <tbody>';
-                        foreach(maquinas($idZona) as $value) {
-                            echo '<tr> <td class="firstColumn">'.$value['patente'].'</td><td>'.$value['fechaRegistro'].'</td><td>'.$value['tara'].'</td><td>'.$value['cargaMaxima'].'</td></tr>';
-                        }
-                        echo '</tbody> </table> </div></div>';
-                    }
-                    foreach(supervisores($idZona) as $value) {
-                       echo '<div class="col-xs-12 col-sm-6 cardContent a"> <div class="col-xs-12"><i class="fa fa-user-circle fa-2x pull-left"></i><p class="text-center montserrat">'.$value['nombreSupervisor'].'</p></div><div class="col-xs-12"><a href="#">Asignar nueva zona</a><a href="#">Eliminar</a> </div></div>';
-                    }
-                echo '</div></div>';
-            }
-        ?>
+<?php
+	include 'recursos/mailer/class.phpmailer.php';
+	$fecha = date('Y-m-d');
+	$hora = date('H:i:s');
+	echo enviarMailRegistroSupervisor('Patricio Villanueva Fuentes','pavillanueva@ing.ucsc.cl','http://www.mmonitors.com');
+	function enviarMailRegistroSupervisor($nombreSupervisor,$emailSupervisor,$link) {
+			$e = new PHPMailer;
+			$e->Host = 'localhost';
+			$e->From = "machmonitor@gmail.com";
+			$e->FromName = 'Machine Monitors';
+			$e->Subject = 'Registro de Supervisores';
+			$e->addAddress($emailSupervisor);
+			$e->MsgHTML('<!DOCTYPE html>
+							<html>
+							    <head>
+							    	<meta charset="UTF-8">
+							    	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+									<style>
+										* {
+											margin: 0;
+											padding: 0;
+											box-sizing: border-box;
+										}
+										body {
+											font-family: Arial;
+										}
+										.letra {
+											font-size: 20px;
+											font-weight: 100;
+											color: #fff;
+										}
+							            .contenedor {
+							                width: 100%;
+							                margin: 0 auto;
+							                overflow: hidden;
+							            }
+							            .rectangulo {
+							                text-align: center;
+							                float: left;
+							                padding: 10px;
+											border-bottom: 5px solid #F5A214;
+											width: 100%;
+											height: 45px;
+											background: #262626;
+							            }
+							            .cuadrado {
+							                width: 100%;
+							                float: left;
+							                padding: 20px 0px 20px 0px;
+							                border-bottom: 5px solid #F5A214;
+							                font-size: 15px;
+							            }
+							        </style>
+							    </head>
+							    <body>
+							    	<div class="contenedor">
+							    		<div class="rectangulo">
+							                <p class="letra">Machine Monitors'.$fecha.$hora'</p>
+                                            
+							            </div>
+							    		<div class="cuadrado">Estimado '.$nombreSupervisor.', para finalizar el registro debes ingresar al siguiente enlance y completar el formulario<br><br><a href='.$link.'>'.$link.'</a><div>
+							        </div>
+							    </body>
+							</html>');
+			if($e->Send()) {
+				return true;
+				}
+			else
+				return false;
+		}
+
+?>
