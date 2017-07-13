e6a500a7b9c1bbec96de3c711672f2045eecb802
monstersoft <pavillanueva@ing.ucsc.cl>
2017-07-13 12:29:08 -0400

    d

--- f.php
+++ f.php
@@ -1,28 +1,31 @@
 <?php
 	include 'recursos/mailer/class.phpmailer.php';
 $mail = new PHPMailer;
-
-//$mail->SMTPDebug = 3;                               // Enable verbose debug output
-
-$mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'smtp1.example.com;smtp2.example.com';  // Specify main and backup SMTP servers
-$mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'machmonitors@gmail.com';                 // SMTP username
-$mail->Password = 'Monsterinc2';                           // SMTP password
-$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
-$mail->Port = 587;                                    // TCP port to connect to
-
-$mail->setFrom('machmonitors@gmail.com', 'Mailer');
-$mail->addAddress('pavillanueva@ing.ucsc.cl', 'Joe User');     // Add a recipient
-$mail->isHTML(true);                                  // Set email format to HTML
-$mail->Subject = 'Here is the subject';
-$mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
-
-if(!$mail->send()) {
-    echo 'Message could not be sent.';
-    echo 'Mailer Error: ' . $mail->ErrorInfo;
-} else {
-    echo 'Message has been sent';
+$mail->isSMTP();
+$mail->Host = "localhost";
+$mail->Port = 25;
+$mail->SMTPSecure = "tls";
+$mail->SMTPOptions = array
+  (
+    'ssl' => array
+    (
+      'verify_peer' => false,
+      'verify_peer_name' => false,
+      'allow_self_signed' => true
+    )
+  );
+$mail->setFrom('machmonitors@ing.ucsc.cl', 'My Server');
+$mail->addAddress('pavillanueva@ing.ucsc.cl', 'My User'); 
+$mail->Subject = 'Message from PHPMailer and Postfix';
+$mail->Body = 'Whatever';
+if ($mail->send())
+// SMTP message send success
+{
+echo 'asdasdasd';
+} 
+else
+// SMTP message send failure
+{
+echo 'askdskjkjaskjsakjsajksasajk';
 }
 ?>
