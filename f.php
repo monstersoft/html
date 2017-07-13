ae999941d677a5bdca9a2fc9b666b4eba3e3ecce
monstersoft <pavillanueva@ing.ucsc.cl>
2017-07-13 12:30:22 -0400

    f

--- f.php
+++ f.php
@@ -1,31 +1,10 @@
 <?php
-	include 'recursos/mailer/class.phpmailer.php';
-$mail = new PHPMailer;
-$mail->isSMTP();
-$mail->Host = "localhost";
-$mail->Port = 25;
-$mail->SMTPSecure = "tls";
-$mail->SMTPOptions = array
-  (
-    'ssl' => array
-    (
-      'verify_peer' => false,
-      'verify_peer_name' => false,
-      'allow_self_signed' => true
-    )
-  );
-$mail->setFrom('machmonitors@ing.ucsc.cl', 'My Server');
-$mail->addAddress('pavillanueva@ing.ucsc.cl', 'My User'); 
-$mail->Subject = 'Message from PHPMailer and Postfix';
-$mail->Body = 'Whatever';
-if ($mail->send())
-// SMTP message send success
-{
-echo 'asdasdasd';
-} 
-else
-// SMTP message send failure
-{
-echo 'askdskjkjaskjsakjsajksasajk';
-}
+// the message
+$msg = "First line of text\nSecond line of text";
+
+// use wordwrap() if lines are longer than 70 characters
+$msg = wordwrap($msg,70);
+
+// send email
+mail("pavillanueva@ing.ucsc.cl","My subject",$msg);
 ?>
