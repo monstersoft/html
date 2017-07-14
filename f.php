8d86d71ccd19fd643e2c404afe33da5669ad201e
monstersoft <pavillanueva@ing.ucsc.cl>
2017-07-14 15:55:55 -0400

    s

--- f.php
+++ f.php
@@ -1,25 +1,6 @@
 <?php
-require 'recrusos/mailer/PHPMailerAutoload.php';
-echo 'asdasdasdasd';
-$mail = new PHPMailer;
-$mail->SMTPDebug = 3;                               // Enable verbose debug output
-$mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'smtp1.example.com;smtp2.example.com';  // Specify main and backup SMTP servers
-$mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'machmonitors@gmail.com';                 // SMTP username
-$mail->Password = 'Monsterinc2';                           // SMTP password
-$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
-$mail->Port = 587;                                    // TCP port to connect to
-$mail->setFrom('machmonitors@gmail.com', 'Mailer');
-$mail->addAddress('pavillanueva@ing.ucsc.cl', 'Patricio Villanuev Fuentes');     // Add a recipient
-$mail->isHTML(true);                                  // Set email format to HTML
-$mail->Subject = 'Here is the subject';
-$mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
-if(!$mail->send()) {
-    echo 'Message could not be sent.';
-    echo 'Mailer Error: ' . $mail->ErrorInfo;
-} else {
-    echo 'Message has been sent';
-}
+    if(mail('pavillanueva@ing.ucsc.cl','BIEN CTM','BIEN RE CTM','From: <Username@Domain.tld>'));
+        echo 'enviado';
+    else
+        echo 'no enviado';
 ?>
