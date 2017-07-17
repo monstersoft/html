d4944c58dd73871f797261d95d75cbdcebfaf491
monstersoft <pavillanueva@ing.ucsc.cl>
2017-07-17 12:15:25 -0400

    s

--- public/cliente/dashboard.php
+++ public/cliente/dashboard.php
@@ -258,7 +258,7 @@
                 dataType: 'json',
                 cache: false,
                 success: function(arreglo) {
-                    $('body').append('<div class="fix2 center2"><i class="fa fa-globe" style="color: #F5A214;"></i> '+arreglo.nombreZona+'<i class="fa fa-calendar" style="color: #F5A214;"></i> '+moment('2017-01-01','YYYY-MM-DD','es').format('dddd Do MMMM  YYYY').toUpperCase()+'<i class="fa fa-cog" style="color: #F5A214;"></i>'+arreglo.patente+'</div>');
+                    $('body').append('<div class="fix2 center2"><i class="fa fa-globe" style="color: #F5A214;"></i> '+arreglo.nombreZona+'<i class="fa fa-calendar" style="color: #F5A214;"></i> '+moment('2017-07-14','YYYY-MM-DD','es').format('dddd Do MMMM  YYYY').toUpperCase()+'<i class="fa fa-cog" style="color: #F5A214;"></i>'+arreglo.patente+'</div>');
                     console.log(JSON.stringify(arreglo));
                 },
                 error: function(xhr) {console.log(xhr.responseText);}
