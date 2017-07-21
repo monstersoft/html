ffb44d2b62fa3f62f42f4768d912fcfcd9fcec07
monstersoft <pavillanueva@ing.ucsc.cl>
2017-07-21 00:06:30 -0400

    D

--- supervisor/js/modalSubirArchivo.js
+++ supervisor/js/modalSubirArchivo.js
@@ -53,7 +53,23 @@ $('.modalSubirArchivo').on('click','#btnSubirArchivo',function(){
             },
             complete: function() {desactivarLoaderBotones('fa-upload','fa-refresh');},
             error: function(xhr) {console.log(xhr.responseText)}
-        });
+        }).fail(function( jqXHR, textStatus, errorThrown ){
+            if (jqXHR.status === 0){
+                alert('No hay coneccion con el servidor');
+            } else if (jqXHR.status == 404) {
+                alert('La pagina solicitada no fue encontrada, error 404');
+            } else if (jqXHR.status == 500) {
+                alert('Error interno del servidor');
+            } else if (textStatus === 'parsererror') {
+                alert('Error en la respuesta, debes analizar la sintaxis JSON');
+            } else if (textStatus === 'timeout') {
+                alert('Ya ha pasado mucho tiempo');
+            } else if (textStatus === 'abort') {
+                alert('La peticion fue abortada');
+            } else {
+                alert('Error desconocido');
+            }
+        });;
     }
     else {
         errorMessage(arreglo);
