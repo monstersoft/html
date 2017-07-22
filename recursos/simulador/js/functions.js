4d04f78e7d3d64a844d2dd465aaeab43a01acce9
monstersoft <pavillanueva@ing.ucsc.cl>
2017-07-22 00:39:25 -0400

    7

--- recursos/simulador/js/functions.js
+++ recursos/simulador/js/functions.js
@@ -1,4 +1,82 @@
 var carpetaRaiz = 'html';
+function generarObjetoLimites() {
+    var formularios = document.forms;
+    var objAux = generarObjetos(formularios.length);
+    for(var i=0;i<formularios.length;i++){
+        for(var j=0;j<formularios[i].elements.length;j++){
+            if(formularios[i].elements[j].classList.contains('valor')) {
+                if(j == 0)
+                    objAux[i].id = formularios[i].elements[j].value;
+                if(j == 1)
+                    objAux[i].lamin = formularios[i].elements[j].value;
+                if(j == 2)
+                    objAux[i].lamax = formularios[i].elements[j].value;
+                if(j == 3)
+                    objAux[i].lomin = formularios[i].elements[j].value;
+                if(j == 4)
+                    objAux[i].lomax = formularios[i].elements[j].value;
+                if(j == 5)
+                    objAux[i].rmin = formularios[i].elements[j].value;
+                if(j == 6)
+                    objAux[i].rmax = formularios[i].elements[j].value;
+                if(j == 7)
+                    objAux[i].gfmin = formularios[i].elements[j].value;
+                if(j == 8)
+                    objAux[i].gfmax = formularios[i].elements[j].value;
+                if(j == 9)
+                    objAux[i].gtmin = formularios[i].elements[j].value;
+                if(j == 10)
+                    objAux[i].gtmax = formularios[i].elements[j].value;
+                if(j == 11)
+                    objAux[i].afmin = formularios[i].elements[j].value;
+                if(j == 12)
+                    objAux[i].afmax = formularios[i].elements[j].value;
+                if(j == 13)
+                    objAux[i].atmin = formularios[i].elements[j].value;
+                if(j == 14)
+                    objAux[i].atmax = formularios[i].elements[j].value;
+                if(j == 15)
+                    objAux[i].cmin = formularios[i].elements[j].value;
+                if(j == 16)
+                    objAux[i].cmax = formularios[i].elements[j].value;
+                if(j == 17)
+                    objAux[i].mf = formularios[i].elements[j].value;
+                if(j == 18)
+                    objAux[i].marcado = formularios[i].elements[j].value;
+                
+            }
+        }
+    }
+    return objAux;
+}
+function generarObjetos(cantidadFormularios) {
+    var aux = 0;
+    var arreglo = []
+    while(aux < cantidadFormularios) {
+        arreglo.push({
+            id: 0,
+            lamin:0,
+            lamax:0,
+            lomin:0,
+            lomax:0,
+            rmin:0,
+            rmax:0,
+            gfmin:0,
+            gfmax:0,
+            gtmin:0,
+            gtmax:0,
+            afmin:0,
+            afmax:0,
+            atmin:0,
+            atmax:0,
+            cmin:0,
+            cmax:0,
+            mf:0
+        });
+        aux++;
+    }
+    return arreglo;
+    }
 function descargar(data,nombreArchivo,idZona){
     var flag = 0;
     var csvData = new Array();
@@ -61,54 +139,6 @@ function generarObjetoDatos(o){
     });
     return objRegistros;
 }
-function generarObjetoLimites() {
-    var formularios = document.forms;
-    var objAux = generarObjetos(formularios.length);
-    for(var i=0;i<formularios.length;i++){
-        for(var j=0;j<formularios[i].elements.length;j++){
-            if(formularios[i].elements[j].classList.contains('valor')) {
-                if(j == 0)
-                    objAux[i].id = formularios[i].elements[j].value;
-                if(j == 1)
-                    objAux[i].lamin = formularios[i].elements[j].value;
-                if(j == 2)
-                    objAux[i].lamax = formularios[i].elements[j].value;
-                if(j == 3)
-                    objAux[i].lomin = formularios[i].elements[j].value;
-                if(j == 4)
-                    objAux[i].lomax = formularios[i].elements[j].value;
-                if(j == 5)
-                    objAux[i].rmin = formularios[i].elements[j].value;
-                if(j == 6)
-                    objAux[i].rmax = formularios[i].elements[j].value;
-                if(j == 7)
-                    objAux[i].gfmin = formularios[i].elements[j].value;
-                if(j == 8)
-                    objAux[i].gfmax = formularios[i].elements[j].value;
-                if(j == 9)
-                    objAux[i].gtmin = formularios[i].elements[j].value;
-                if(j == 10)
-                    objAux[i].gtmax = formularios[i].elements[j].value;
-                if(j == 11)
-                    objAux[i].afmin = formularios[i].elements[j].value;
-                if(j == 12)
-                    objAux[i].afmax = formularios[i].elements[j].value;
-                if(j == 13)
-                    objAux[i].atmin = formularios[i].elements[j].value;
-                if(j == 14)
-                    objAux[i].atmax = formularios[i].elements[j].value;
-                if(j == 15)
-                    objAux[i].cmin = formularios[i].elements[j].value;
-                if(j == 16)
-                    objAux[i].cmax = formularios[i].elements[j].value;
-                if(j == 17)
-                    objAux[i].mf = formularios[i].elements[j].value;
-                
-            }
-        }
-    }
-    return objAux;
-}
 function r(l,h,d) {
     if($.isNumeric(l) && $.isNumeric(h)){
         if(l == 0 && h == 0)
@@ -122,34 +152,6 @@ function r(l,h,d) {
     else
         return '';
 }
-function generarObjetos(cantidadFormularios) {
-    var aux = 0;
-    var arreglo = []
-    while(aux < cantidadFormularios) {
-        arreglo.push({
-            id: 0,
-            lamin:0,
-            lamax:0,
-            lomin:0,
-            lomax:0,
-            rmin:0,
-            rmax:0,
-            gfmin:0,
-            gfmax:0,
-            gtmin:0,
-            gtmax:0,
-            afmin:0,
-            afmax:0,
-            atmin:0,
-            atmax:0,
-            cmin:0,
-            cmax:0,
-            mf:0
-        });
-        aux++;
-    }
-    return arreglo;
-    }
 function devuelveUrl(pathSinCarpetaRaiz) {
     var url;
     var host = window.location.host;
--- recursos/simulador/php/zonas.php
+++ recursos/simulador/php/zonas.php
@@ -2,12 +2,23 @@
 	include("conexion.php");
     $c = conectar();
     $a = array();
+    $b = array();
+    $arr = array();
     $q = 'SELECT * FROM zonas';
     if($re = mysqli_query($c,$q)) {
         while($r = mysqli_fetch_assoc($re)) {
             array_push($a,array('idZona' => $r['idZona'],'idEmpresa' => $r['idEmpresa'], 'nombre' => utf8_encode($r['nombre'])));
         }
     }
+    $idZona = $a[0]['idZona'];
+    $q = "SELECT maquinas.idMaquina, maquinas.patente FROM maquinas WHERE maquinas.idZona = '$idZona'";
+    if($re = mysqli_query($c,$q)) {
+        while($r = mysqli_fetch_assoc($re)) {
+            array_push($b,array('idMaquina' => $r['idMaquina'],'patente' => $r['patente']));
+        }
+    }
+    $arr['zonas'] = $a;
+    $arr['maquinas'] = $b;
     mysqli_close($c);
-    echo json_encode($a);
+    echo json_encode($arr);
 ?>
