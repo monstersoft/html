1f7ecd6b9bf41ea42aa0f57e28962e2b872fcb2c
monstersoft <pavillanueva@ing.ucsc.cl>
2017-07-24 23:18:00 -0400

    S

--- f.php
+++ f.php
@@ -1,26 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Document</title>
-    <link rel="stylesheet" href="recursos/bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="recursos/pickadate/default.css">
-    <link rel="stylesheet" href="recursos/pickadate/classic.time.css">
-    <link rel="stylesheet" href="recursos/pickadate/classic.date.css">
-</head>
-    <div class="col-xs-12" id="cont"><input type="text" class="datepicker"></div>
-<body>
-    <script src="recursos/jquery/jquery.min.js"></script>
-    <script src="recursos/bootstrap/js/bootstrap.min.js"></script>
-    <script src="recursos/pickadate/picker.js"></script>
-    <script src="recursos/pickadate/picker.time.js"></script>
-    <script src="recursos/pickadate/picker.date.js"></script>
-    <script>
-        $('.datepicker').pickadate({
-  container: 'body'
-})
-    
-    
-    </script>
-</body>
-</html>
+<?php
+    include 'php/funciones.php';
+    debug($_SERVER);
+?>
