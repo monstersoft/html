aefe692e4333642196f8d060cc29149e065d7468
monstersoft <pavillanueva@ing.ucsc.cl>
2017-07-25 00:16:11 -0400

    S

--- php/route.php
+++ php/route.php
@@ -1,8 +1,8 @@
 <?php
-    define ("CARPETA","html");
     function raiz() {
+        define('CARPETA_XAMPP','html');
         if($_SERVER['HTTP_HOST'] == 'localhost')
-            return $_SERVER['REQUEST_SCHEME'].'://'.CARPETA;
+            return $_SERVER['REQUEST_SCHEME'].'://'.$_SERVER['HTTP_HOST'].'/'.CARPETA_XAMPP;
         else
             return $_SERVER['REQUEST_SCHEME'].'://'.$_SERVER['SERVER_NAME'];
     }
