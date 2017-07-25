3177a4f31b26f00bd29b0dc42f405dce7922ba93
monstersoft <pavillanueva@ing.ucsc.cl>
2017-07-25 15:46:44 -0400

    s

--- js/reestablecer.js
+++ js/reestablecer.js
@@ -9,7 +9,7 @@ $('#btnReestablecer').click(function() {
         arreglo.push('<div class="item">Correo no está en un  formado adecuado</div>');
     if(maxLength(email, 60))
         arreglo.push('<div class="item">Correo no debe superar los 60 caracteres</div>');
-   // if(arreglo.length == 0) {
+    if(arreglo.length == 0) {
         $.ajax({                  
             url: devuelveUrl('ajax/generaLink.php'),
             data: {txtCorreo: $('#txtCorreo').val()},
@@ -30,13 +30,15 @@ $('#btnReestablecer').click(function() {
                 else
                     errorUi('<div class="item">El correo no está registrado en el sistema</div>');
             },
-            error: function(xhr) {console.log(xhr.responseText); alert('error'); }
+            error: function(xhr) {console.log(xhr.responseText);}
         }).complete(function(){
             $('#btnReestablecer').removeClass('disabled');
             $('#btnReestablecer i').removeClass('fa-cog fa-spin').addClass('fa-send');
         }).fail(function( jqXHR, textStatus, errorThrown ){
-            alert('fail');
-            /*if (jqXHR.status === 0){
+            console.log(jqXHR);
+            console.log(textStatus);
+            console.log(errorThrown);
+            if (jqXHR.status === 0){
                 alert('No hay coneccion con el servidor, debe comunicarte con el administrador');
             } else if (jqXHR.status == 404) {
                 alert('La pagina solicitada no fue encontrada: error 404, debes comunicarte con el administrador');
@@ -50,9 +52,9 @@ $('#btnReestablecer').click(function() {
                 alert('La peticion fue abortada, debes comunicarte con el administrador');
             } else {
                 alert('Error desconocido, debes comunicarte con el administrador');
-            }*/
+            }
         });
-    //}
-    //else
-      //  errorUi(arreglo);
+    }
+    else
+        errorUi(arreglo);
 });
