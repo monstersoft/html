ba3c94cf59fcbaffbf540afc4af5ccd29279d07a
monstersoft <pavillanueva@ing.ucsc.cl>
2017-07-26 06:16:19 -0400

    s

--- 0/cliente/js/modalEliminarEmpresa.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    $('.eliminarEmpresa').click(function(){
-        $('.ui.negative.message').remove();
-        $('.ui.warning.message').remove();
-        $('.ui.icon.success.message').remove();
-        $('.modalEliminarEmpresa').modal('show');
-    });
-});
--- 0/supervisor/subirArchivo.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $response = array();
-    $response['fechaDatos'] = $_POST['fechaDatos'];
-    $response['archivoZona'] = $_FILES['archivoZona']['tmp_name'];
-    $response['archivoZonaText'] = $_POST['archivoZonaText'];
-    $response['idZonaArchivo'] = $_POST['idZonaArchivo'];
-
-    echo json_encode($response);
-?>
