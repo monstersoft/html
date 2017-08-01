32c48b5a6dda478a8fcb06b2c732b61a64fd1a4a
monstersoft <pavillanueva@ing.ucsc.cl>
2017-08-01 06:55:40 -0400

    s

---
+++ errores/404.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
+        <meta name="theme-color" content="#262626" />
+        <title>Machine Monitors</title>
+        <link rel="stylesheet" href="http://www.mmonitors.com/recursos/semantic/semantic.css">
+        <link rel="stylesheet" href="http://www.mmonitors.com/recursos/awesome/css/font-awesome.min.css">
+        <link rel="stylesheet" href="http://www.mmonitors.com/css/index.css">
+    </head>
+    <body>
+        <div class="ui aligned center aligned grid">
+            <div class="margen column">
+                <h2 class="ui icon header">
+                    <i class="settings icon" style="color: #F5A214;"></i>
+                    <div class="content montserrat" style="color: white;">
+                    Machine Monitors
+                        <div class="sub header montserrat" style="color: white;">Plan de vigilancia de maquinaria pesada</div>
+                    </div>
+                </h2>
+                <h1 class="titulo montserrat cent"><i class="fa fa-exclamation-circle" style="margin-right: 20px;"></i>ERROR 404</h1>
+                <h6 class="titulo montserrat cent">Page not found</h6>
+            </div>
+        </div>
+        <script src="http://www.mmonitors.com/recursos/jquery/jquery.min.js"></script>
+        <script src="http://www.mmonitors.com/recursos/semantic/semantic.min.js"></script>
+    </body>
+</html>
