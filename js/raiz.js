89951620ab53bd63287bf19dd937db00519b8c7b
monstersoft <pavillanueva@ing.ucsc.cl>
2017-08-02 21:26:54 -0400

    s

--- js/raiz.js
+++
@@ -1,7 +0,0 @@
-const CARPETA_XAMPP = 'html';
-function raiz() {
-    if(location.host == 'localhost')
-        return location.origin+'/'+CARPETA_XAMPP;
-    else
-        return location.origin;
-}
--- php/raiz.php
+++
@@ -1,9 +0,0 @@
-<?php
-    function raiz() {
-        define('CARPETA_XAMPP','html');
-        if($_SERVER['HTTP_HOST'] == 'localhost')
-            return $_SERVER['REQUEST_SCHEME'].'://'.$_SERVER['HTTP_HOST'].'/'.CARPETA_XAMPP.'/';
-        else
-            return $_SERVER['REQUEST_SCHEME'].'://'.$_SERVER['SERVER_NAME'].'/';
-    }
-?>
