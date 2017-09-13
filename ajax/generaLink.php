7b6c94b811fe9e37e996ec93730820e3662d349b
monstersoft <pavillanueva@ing.ucsc.cl>
2017-09-13 01:57:06 -0300

    s

--- ajax/generaLink.php
+++ ajax/generaLink.php
@@ -83,7 +83,7 @@
         $e->SMTPSecure = 'tls';
         $e->SMTPAuth = true;
         $e->Username = "mmonitors17@gmail.com";
-        $e->Password = "Monsterinc2";
+        $e->Password = "Monsterinc3";
         $e->FromName = "Machine Monitors";
         $e->addAddress($emailSupervisor);
         $e->Subject = 'Reestablecer Contraseña';
