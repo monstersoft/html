76c7cb942cb88f0ef00164f3dc51b7f73355902a
monstersoft <pavillanueva@ing.ucsc.cl>
2017-12-18 20:43:03 -0300

    w

--- send.php
+++ send.php
@@ -1,26 +1,28 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <link rel="stylesheet" href="recursos/bootstrap/css/bootstrap.css">
-    <title>Document</title>
+<meta charset="UTF-8">
+<title>Simple Image Gallery</title>
+<style type="text/css">
+    .img-box{
+        display: inline-block;
+        text-align: center;
+        margin: 0 15px;
+    }
+</style>
 </head>
 <body>
-    <button class="btn btn-large" id="sta">SAVE TEXT AS</button>
-    <button class="btn btn-large" id="sa">SAVE AS HTML 5</button>
-    <script src="recursos/jquery/jquery.min.js"></script>
-    <script src="recursos/fileSaver/FileSaver.min.js"></script>
-    <script>
-        $(document).ready(function(){
-            $('#sta').click(function(){
-                saveTextAs("Hi","test.csv");
-            });
-            $('#sa').click(function(){
-                var blob = new Blob(['jajaja'], {type: "text/plain;charset=utf-8"});
-                saveAs(blob,"hello worlld");
-            });
-        });
-    </script>
+    <?php
+    // Array containing sample image file names
+    $images = array("kites.jpg", "balloons.jpg");
+    
+    // Loop through array to create image gallery
+    foreach($images as $image){
+        echo '<div class="img-box">';
+            echo '<img src="images/' . $image . '" width="200" alt="' .  pathinfo($image, PATHINFO_FILENAME) .'">';
+            echo '<p><a href="download.php?file=' . urlencode($image) . '">Download</a></p>';
+        echo '</div>';
+    }
+    ?>
 </body>
 </html>
