2f3ccabe75df611cb189ca26ded5d0cc4456ef65
monstersoft <pavillanueva@ing.ucsc.cl>
2017-12-18 20:59:36 -0300

    s

--- send.php
+++ send.php
@@ -1,28 +1,40 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<meta charset="UTF-8">
-<title>Simple Image Gallery</title>
-<style type="text/css">
-    .img-box{
-        display: inline-block;
-        text-align: center;
-        margin: 0 15px;
-    }
-</style>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <link rel="stylesheet" href="recursos/bootstrap/css/bootstrap.css">
+    <title>Document</title>
 </head>
 <body>
-    <?php
-    // Array containing sample image file names
-    $images = array("kites.jpg", "balloons.jpg");
+    <div class="jumbotron"><button class="btn btn-large" id="sta">SAVE TEXT AS</button></div>
+    <script src="recursos/jquery/jquery.min.js"></script>
     
-    // Loop through array to create image gallery
-    foreach($images as $image){
-        echo '<div class="img-box">';
-            echo '<img src="images/' . $image . '" width="200" alt="' .  pathinfo($image, PATHINFO_FILENAME) .'">';
-            echo '<p><a href="download.php?file=' . urlencode($image) . '">Download</a></p>';
-        echo '</div>';
-    }
-    ?>
+    <script src="recursos/fileSaver/FileSaver.min.js"></script>
+    <script>
+        $(document).ready(function(){
+            $('#sta').click(function(){
+var saveData = (function () {
+    var a = document.createElement("a");
+    document.body.appendChild(a);
+    a.style = "display: none";
+    return function (data, fileName) {
+        var json = JSON.stringify(data),
+            blob = new Blob([json], {type: "octet/stream"}),
+            url = window.URL.createObjectURL(blob);
+        a.href = url;
+        a.download = fileName;
+        a.click();
+        window.URL.revokeObjectURL(url);
+    };
+}());
+
+var data = { x: 42, s: "hello, world", d: new Date() },
+    fileName = "my-download.json";
+
+saveData(data, fileName);
+            });
+        });
+    </script>
 </body>
 </html>
