9e4e16b706bbc5fd400bf5f8bf5bd64a0a1bd9bd
monstersoft <pavillanueva@ing.ucsc.cl>
2017-12-18 20:13:06 -0300

    d

--- send.php
+++ send.php
@@ -5,7 +5,7 @@
     <title>Document</title>
 </head>
 <body>
-   <a href="#" id="click">click</a>
+    <a href="#" id="btn-save">sads</a>
     <script src="recursos/jquery/jquery.min.js"></script>
     <script src="a.js"></script>
 </body>
