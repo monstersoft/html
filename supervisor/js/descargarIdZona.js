1ec9bb4d5d6919dad11ddc48d83a326efcc67a56
monstersoft <pavillanueva@ing.ucsc.cl>
2017-12-18 17:56:47 -0300

    s

--- supervisor/js/descargarIdZona.js
+++ supervisor/js/descargarIdZona.js
@@ -7,6 +7,6 @@ $('.descargarId').click(function(){
     $(name).attr('href',window.URL.createObjectURL(blob));
     $(name).attr('download','idZona'+$(this).attr('id')+'.csv');*/
     buffer = $(this).attr('id');
-    var blob = new Blob([buffer], {"type": "text/csv;charset=utf8;"});
+    var blob = new File([buffer], {"type": "text/csv;charset=utf8;"});
     saveAs(blob,'idZona'+buffer);
 });
