0274d64f3d464a375c36bcd06a6ec74d9ad72646
monstersoft <pavillanueva@ing.ucsc.cl>
2017-12-19 01:16:54 -0300

    sss

--- send.php
+++ send.php
@@ -1,37 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <link rel="stylesheet" href="recursos/bootstrap/css/bootstrap.css">
-    <title>Document</title>
-</head>
-<body>
-    <div class="jumbotron"><button class="btn btn-large" id="sta">SAVE TEXT AS</button></div>
-    <script src="recursos/jquery/jquery.min.js"></script>
-    
-    <script src="recursos/fileSaver/FileSaver.min.js"></script>
-    <script>
-        $(document).ready(function(){
-            $('#sta').click(function(){
-var saveData = (function () {
-    var a = document.createElement("a");
-    document.body.appendChild(a);
-    a.style = "display: none";
-    return function (data, fileName) {
-        var json = JSON.stringify(data),
-            blob = new Blob([json], {type: "text/csv"}),
-            url = window.URL.createObjectURL(blob);
-        a.href = url;
-        a.download = fileName;
-        a.click();
-        window.URL.revokeObjectURL(url);
-    };
-}());
+<?php
+  // Creates a new csv file and store it in tmp directory
+  $new_csv = fopen('/tmp/report.csv', 'w');
+  fputcsv($new_csv, $row);
+  fclose($new_csv);
 
-saveData(20, "asdasdsd.csv");
-            });
-        });
-    </script>
-</body>
-</html>
+  // output headers so that the file is downloaded rather than displayed
+  header("Content-type: text/csv");
+  header("Content-disposition: attachment; filename = report.csv");
+  readfile("/tmp/report.csv");
+?>
